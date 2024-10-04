9f96d07a57b733f26121ef1f97a89e5118baf8db
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-04 21:25:38 +0200

    fixes
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/utils/codecs/CodecUtils.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/utils/codecs/CodecUtils.kt
@@ -5,9 +5,10 @@ import com.mojang.serialization.Codec
 
 object CodecUtils {
 
-    fun <T> set(codec: Codec<T>): Codec<Set<T>> = codec.listOf().xmap({ HashSet(it) }, { ArrayList(it) })
+    fun <T> set(codec: Codec<T>): Codec<MutableSet<T>> = codec.listOf().xmap({ it.toMutableSet() }, { it.toList() })
 
-    fun <T> list(codec: Codec<T>): Codec<List<T>> = codec.listOf().xmap({ ArrayList(it) }, { it })
+    fun <T> list(codec: Codec<T>): Codec<MutableList<T>> = codec.listOf().xmap({ it.toMutableList() }, { it })
 
-    fun <A, B> map(key: Codec<A>, value: Codec<B>): Codec<Map<A, B>> = Codec.unboundedMap(key, value).xmap({ it.toMutableMap() }, { it })
+    fun <A, B> map(key: Codec<A>, value: Codec<B>): Codec<MutableMap<A, B>> =
+        Codec.unboundedMap(key, value).xmap({ it.toMutableMap() }, { it })
 }
