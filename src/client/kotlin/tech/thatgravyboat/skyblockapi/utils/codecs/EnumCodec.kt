b9d0ff1aa9043b7c234a9d6d29a0a16270e8782a
ThatGravyBoat <thatgravyboat@gmail.com>
2024-10-05 08:23:50 -0230

    Fix enums in codecs part 2

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/utils/codecs/EnumCodec.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/utils/codecs/EnumCodec.kt
@@ -5,7 +5,7 @@ import com.mojang.serialization.Codec
 import com.mojang.serialization.DataResult
 import com.mojang.serialization.DynamicOps
 
-class EnumCodec<T : Enum<T>> private constructor(private val codec: Codec<T>) : Codec<T> {
+class EnumCodec<T> private constructor(private val codec: Codec<T>) : Codec<T> {
 
     override fun <T1 : Any?> encode(input: T, ops: DynamicOps<T1>?, prefix: T1): DataResult<T1> = codec.encode(input, ops, prefix)
     override fun <T1 : Any?> decode(ops: DynamicOps<T1>?, input: T1): DataResult<Pair<T, T1>> = codec.decode(ops, input)
@@ -15,7 +15,10 @@ class EnumCodec<T : Enum<T>> private constructor(private val codec: Codec<T>) : 
         fun <T : Enum<T>> of(constants: Array<T>): EnumCodec<T> =
             EnumCodec(Codec.withAlternative(constantCodec(constants), intCodec(constants)))
 
-        private fun <T : Enum<T>> intCodec(constants: Array<T>): Codec<T> {
+        internal fun <T> forKCodec(constants: Array<T>): EnumCodec<T> =
+            EnumCodec(Codec.withAlternative(constantCodec(constants), intCodec(constants)))
+
+        private fun <T> intCodec(constants: Array<T>): Codec<T> {
             return Codec.INT.flatXmap(
                 { ordinal: Int ->
                     if (ordinal >= 0 && ordinal < constants.size) {
@@ -23,19 +26,19 @@ class EnumCodec<T : Enum<T>> private constructor(private val codec: Codec<T>) : 
                     }
                     DataResult.error { "Unknown enum ordinal: $ordinal" }
                 },
-                { value: T -> DataResult.success(value.ordinal) },
+                { value: T -> DataResult.success((value as Enum<*>).ordinal) },
             )
         }
 
-        private fun <T : Enum<T>> constantCodec(constants: Array<T>): Codec<T> = Codec.STRING.flatXmap(
+        private fun <T> constantCodec(constants: Array<T>): Codec<T> = Codec.STRING.flatXmap(
             { name: String ->
                 runCatching {
-                    DataResult.success(constants.first { it.name == name })
+                    DataResult.success(constants.first { (it as Enum<*>).name == name })
                 }.getOrElse {
                     DataResult.error { "Unknown enum name: $name" }
                 }
             },
-            { value: T -> DataResult.success(value.name) },
+            { value: T -> DataResult.success((value as Enum<*>).name) },
         )
     }
 
