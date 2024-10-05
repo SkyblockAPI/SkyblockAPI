888ce827278ae4f3767c618cf45f5c2da71a83f8
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-05 13:38:55 +0200

    readd that
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/profile/CurrencyAPI.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/profile/CurrencyAPI.kt
@@ -5,6 +5,7 @@ import tech.thatgravyboat.skyblockapi.api.events.info.ScoreboardUpdateEvent
 import tech.thatgravyboat.skyblockapi.api.events.info.TabWidget
 import tech.thatgravyboat.skyblockapi.api.events.info.TabWidgetChangeEvent
 import tech.thatgravyboat.skyblockapi.api.events.profile.ProfileChangeEvent
+import tech.thatgravyboat.skyblockapi.api.location.LocationAPI
 import tech.thatgravyboat.skyblockapi.api.location.SkyblockIsland
 import tech.thatgravyboat.skyblockapi.modules.Module
 import tech.thatgravyboat.skyblockapi.utils.extentions.parseFormattedDouble
@@ -26,6 +27,8 @@ object CurrencyAPI {
     private val currencyGroup = RegexGroup.SCOREBOARD.group("currency")
     private val purseRegex = currencyGroup.create("purse", "(?:Purse|Piggy): (?<purse>[\\d,kmb.]+)")
     private val motesRegex = currencyGroup.create("motes", "Motes: (?<motes>[\\d,kmb]+)")
+    private val copperRegex = currencyGroup.create("copper", "Copper: (?<copper>[\\d,kmb]+)")
+    private val northStarsRegex = currencyGroup.create("northstars", "North Stars: (?<northstars>[\\d,kmb]+)")
 
     var purse: Double = 0.0
         private set
@@ -67,13 +70,29 @@ object CurrencyAPI {
 
     @Subscription
     fun onScoreboardChange(event: ScoreboardUpdateEvent) {
-        if (SkyblockIsland.THE_RIFT.inIsland()) {
-            motesRegex.anyMatch(event.added, "motes") { (motes) ->
-                this.motes = motes.parseFormattedLong()
+        when (LocationAPI.island) {
+            SkyblockIsland.THE_RIFT -> {
+                motesRegex.anyMatch(event.added, "motes") { (motes) ->
+                    this.motes = motes.parseFormattedLong()
+                }
             }
-        } else {
-            purseRegex.anyMatch(event.added, "purse") { (purse) ->
-                this.purse = purse.parseFormattedDouble()
+
+            SkyblockIsland.GARDEN -> {
+                copperRegex.anyMatch(event.added, "purse") { (purse) ->
+                    this.purse = purse.parseFormattedDouble()
+                }
+            }
+
+            SkyblockIsland.JERRYS_WORKSHOP -> {
+                northStarsRegex.anyMatch(event.added, "northstars") { (northstars) ->
+                    this.northStars = northstars.parseFormattedLong()
+                }
+            }
+
+            else -> {
+                purseRegex.anyMatch(event.added, "purse") { (purse) ->
+                    this.purse = purse.parseFormattedDouble()
+                }
             }
         }
     }
