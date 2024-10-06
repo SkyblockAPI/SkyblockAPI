4fe97d9d6854792106eb2098cca417b92e3b1301
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-06 21:47:43 +0200

    hollowsapi
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

---
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/area/mining/HollowsAPI.kt
@@ -0,0 +1,36 @@
+package tech.thatgravyboat.skyblockapi.api.area.mining
+
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.info.ScoreboardUpdateEvent
+import tech.thatgravyboat.skyblockapi.api.location.SkyblockIsland
+import tech.thatgravyboat.skyblockapi.modules.Module
+import tech.thatgravyboat.skyblockapi.utils.regex.RegexGroup
+import tech.thatgravyboat.skyblockapi.utils.regex.RegexUtils.anyMatch
+
+@Module
+object HollowsAPI {
+    private val glaciteGroup = RegexGroup.SCOREBOARD.group("mining.hollows")
+
+    var heat: Int? = null
+        private set
+
+    var rawHeat = ""
+        private set
+
+    // Heat: IMMUNE
+    // Heat: 24♨
+    private val heatPattern = glaciteGroup.create("heat", "Heat: (?<raw>.*(?<heat>\\d+|IMMUNE)♨?)")
+
+    @Subscription
+    fun onScoreboardUpdate(event: ScoreboardUpdateEvent) {
+        if (!SkyblockIsland.CRYSTAL_HOLLOWS.inIsland()) return
+
+        heatPattern.anyMatch(event.added, "raw", "heat") { (raw, heat) ->
+            this.rawHeat = raw
+            this.heat = heat.toIntOrNull()
+
+            println("HollowsAPI: $rawHeat")
+            println("HollowsAPI: $heat")
+        }
+    }
+}
