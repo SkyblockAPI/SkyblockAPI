14b8b02e0335750a66f4f67deaec90938b3bd7c8
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-10-06 23:11:54 +0200

    add mineshaft
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/location/SkyblockIsland.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/location/SkyblockIsland.kt
@@ -10,6 +10,9 @@ enum class SkyblockIsland(val id: String) {
     DEEP_CAVERNS("mining_2"),
     DWARVEN_MINES("mining_3"),
     CRYSTAL_HOLLOWS("crystal_hollows"),
+
+    // todo: check if this is correct, how do I check?
+    MINESHAFT("mineshaft"),
     SPIDERS_DEN("combat_1"),
     THE_END("combat_3"),
     CRIMSON_ISLE("crimson_isle"),
