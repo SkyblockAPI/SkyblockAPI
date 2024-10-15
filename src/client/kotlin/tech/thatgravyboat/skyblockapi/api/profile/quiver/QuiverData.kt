b66330903f407f251fb69896b3fea34b0a228a9c
Empa <itsempa@users.noreply.github.com>
2024-10-15 13:33:57 +0200

    make constructor with no parameters for quiverdata

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/profile/quiver/QuiverData.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/profile/quiver/QuiverData.kt
@@ -5,9 +5,11 @@ import tech.thatgravyboat.skyblockapi.kcodec.GenerateCodec
 
 @GenerateCodec
 data class QuiverData(
-    var current: String? = null,
+    var current: String?,
     val arrows: MutableMap<String, Int> = mutableMapOf()
 ) {
+    constructor() : this(null)
+
     companion object {
         val CODEC = KCodec.getCodec<QuiverData>()
     }
