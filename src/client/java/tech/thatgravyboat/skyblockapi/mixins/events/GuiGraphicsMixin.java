62d6d83d4c2f7626901b74b9be59241d5cbdc8df
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-11-23 11:07:56 +0100

    comment out
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/java/tech/thatgravyboat/skyblockapi/mixins/events/GuiGraphicsMixin.java
+++ src/client/java/tech/thatgravyboat/skyblockapi/mixins/events/GuiGraphicsMixin.java
@@ -4,11 +4,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
-import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipPositioner;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -16,11 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import tech.thatgravyboat.skyblockapi.api.SkyBlockAPI;
-import tech.thatgravyboat.skyblockapi.api.events.minecraft.ui.GatherItemTooltipComponentsEvent;
 import tech.thatgravyboat.skyblockapi.api.events.render.RenderItemBarEvent;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Mixin(GuiGraphics.class)
 public class GuiGraphicsMixin {
@@ -57,7 +49,7 @@ public class GuiGraphicsMixin {
         lastStack.set(stack);
     }
 
-    @WrapOperation(
+    /*@WrapOperation(
         method = "renderTooltip(Lnet/minecraft/client/gui/Font;Ljava/util/List;Ljava/util/Optional;IILnet/minecraft/resources/ResourceLocation;)V",
         at = @At(
             value = "INVOKE",
@@ -78,5 +70,5 @@ public class GuiGraphicsMixin {
         GatherItemTooltipComponentsEvent event = new GatherItemTooltipComponentsEvent(lastStack.get(), listCopy);
         event.post(SkyBlockAPI.getEventBus());
         operation.call(instance, font, listCopy, i, j, positioner, texture);
-    }
+    }*/
 }
