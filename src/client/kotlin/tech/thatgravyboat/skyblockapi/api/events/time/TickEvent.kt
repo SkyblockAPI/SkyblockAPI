9b9352e0bb7bc1896a0d4b5688ddee8e819aeba5
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-12-05 19:00:22 +0100

    add ticks in tickevent
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/events/time/TickEvent.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/events/time/TickEvent.kt
@@ -1,5 +1,15 @@
 package tech.thatgravyboat.skyblockapi.api.events.time
 
+import tech.thatgravyboat.skyblockapi.api.events.base.EventBus
 import tech.thatgravyboat.skyblockapi.api.events.base.SkyBlockEvent
 
-object TickEvent : SkyBlockEvent()
+object TickEvent : SkyBlockEvent() {
+    var ticks = 0
+        private set
+
+    override fun post(bus: EventBus): Boolean {
+        ticks++
+        return super.post(bus)
+    }
+}
+
