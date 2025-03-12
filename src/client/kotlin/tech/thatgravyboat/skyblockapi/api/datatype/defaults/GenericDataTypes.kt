fce015efda9f1bb455b3552e673f1a6243f6b66b
Morazzer <59416038+Morazzer@users.noreply.github.com>
2025-03-12 12:54:29 +0100

    feat: add attribute support

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/datatype/defaults/GenericDataTypes.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/datatype/defaults/GenericDataTypes.kt
@@ -29,6 +29,11 @@ object GenericDataTypes {
     }
     val POTION: DataType<String> = DataType("potion") { it.getTag("potion")?.asString }
     val POTION_LEVEL: DataType<Int> = DataType("potion_level") { it.getTag("potion_level")?.asInt }
+    val ATTRIBUTES: DataType<Map<String, Int>> = DataType("attributes") {
+        it.getTag("attributes")?.asObject?.let { tag ->
+            buildMap { tag.allKeys.forEach { key -> this[key] = tag.getInt(key) } }
+        }
+    }
 
     @Subscription
     fun onDataTypeRegistration(event: RegisterDataTypesEvent) {
@@ -43,6 +48,7 @@ object GenericDataTypes {
         event.register(ENCHANTMENTS)
         event.register(POTION)
         event.register(POTION_LEVEL)
+        event.register(ATTRIBUTES)
     }
 
     private val Tag.asInt get() = (this as? NumericTag)?.asInt
