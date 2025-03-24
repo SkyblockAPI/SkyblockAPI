a2cf0dc8a3dc17cd1e17f4d99753ea3155bb6a49
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-24 15:25:53 +0100

    fix: getting uuid for parts with no uuid what the fuck hypixel
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/datatype/defaults/GenericDataTypes.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/datatype/defaults/GenericDataTypes.kt
@@ -1,6 +1,7 @@
 package tech.thatgravyboat.skyblockapi.api.datatype.defaults
 
 import kotlinx.datetime.Instant
+import net.minecraft.Util
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.NumericTag
 import net.minecraft.nbt.Tag
@@ -74,6 +75,7 @@ object GenericDataTypes {
 
     private fun getFishingRodPartDataType(name: String) = DataType(name) {
         val tag = it.getTag(name)?.asObject ?: return@DataType null
-        JUUID.fromString(tag.getString("uuid")) to tag.getString("part")
+        val uuid = runCatching { JUUID.fromString(tag.getString("uuid")) }.getOrNull() ?: Util.NIL_UUID
+        uuid to tag.getString("part")
     }
 }
