d589e8da383f9de25356a411c0ad76dd913dbaff
Gravy Boat <gravy@thatgravyboat.tech>
2025-04-15 07:34:13 -0230

    feat: add floor number to DungeonFloor.kt

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/area/dungeon/DungeonFloor.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/area/dungeon/DungeonFloor.kt
@@ -1,24 +1,30 @@
 package tech.thatgravyboat.skyblockapi.api.area.dungeon
 
-enum class DungeonFloor(val bossName: String, val chatBossName: String = bossName) {
-    E("The Watcher"),
+enum class DungeonFloor(
+    val bossName: String,
+    val chatBossName: String = bossName,
+    val floorNumber: Int,
+) {
+    E("The Watcher", 0),
 
-    F1("Bonzo"),
-    F2("Scarf"),
-    F3("The Professor"),
-    F4("Thorn"),
-    F5("Livid"),
-    F6("Sadan"),
-    F7("Necron", "Maxor"),
+    F1("Bonzo", 1),
+    F2("Scarf", 2),
+    F3("The Professor", 3),
+    F4("Thorn", 4),
+    F5("Livid", 5),
+    F6("Sadan", 6),
+    F7("Necron", "Maxor", 7),
 
-    M1("Bonzo"),
-    M2("Scarf"),
-    M3("The Professor"),
-    M4("Thorn"),
-    M5("Livid"),
-    M6("Sadan"),
-    M7("Necron", "Maxor"),
+    M1("Bonzo", 1),
+    M2("Scarf", 2),
+    M3("The Professor", 3),
+    M4("Thorn", 4),
+    M5("Livid", 5),
+    M6("Sadan", 6),
+    M7("Necron", "Maxor", 7),
     ;
+
+    constructor(bossName: String, floorNumber: Int) : this(bossName, bossName, floorNumber)
 
     companion object {
         fun getByName(name: String) = runCatching { DungeonFloor.valueOf(name) }.getOrNull()
