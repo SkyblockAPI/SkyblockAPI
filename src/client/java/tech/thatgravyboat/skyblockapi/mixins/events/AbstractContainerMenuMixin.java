df71b3a8d630f6f1b2ed7303a3caaa4a929454fd
mona <59416038+meowora@users.noreply.github.com>
2025-04-21 18:57:43 +0200

    feat: visual item replacements

---
+++ src/client/java/tech/thatgravyboat/skyblockapi/mixins/events/AbstractContainerMenuMixin.java
@@ -0,0 +1,36 @@
+package tech.thatgravyboat.skyblockapi.mixins.events;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import tech.thatgravyboat.skyblockapi.api.events.screen.InventoryChangeEvent;
+import tech.thatgravyboat.skyblockapi.helpers.McScreen;
+
+@Mixin(AbstractContainerMenu.class)
+public class AbstractContainerMenuMixin {
+
+    @Shadow
+    @Final
+    public int containerId;
+
+    @WrapOperation(method = {"initializeContents", "setItem"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/Slot;set(Lnet/minecraft/world/item/ItemStack;)V"))
+    public void setItemEvent(Slot instance, ItemStack itemStack, Operation<Void> original) {
+        original.call(instance, itemStack);
+        final AbstractContainerScreen<?> asMenu = McScreen.INSTANCE.getAsMenu();
+        if (asMenu == null) {
+            return;
+        }
+        if (asMenu.getMenu().containerId != containerId) {
+            return;
+        }
+        new InventoryChangeEvent(itemStack, instance, asMenu.getTitle()).post$skyblock_api_client();
+    }
+
+}
--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/events/screen/ContainerChangeEvent.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/events/screen/ContainerChangeEvent.kt
@@ -2,16 +2,29 @@ package tech.thatgravyboat.skyblockapi.api.events.screen
 
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
 import net.minecraft.network.chat.Component
+import net.minecraft.world.entity.player.Inventory
+import net.minecraft.world.inventory.Slot
 import net.minecraft.world.item.ItemStack
 import tech.thatgravyboat.skyblockapi.api.events.base.SkyBlockEvent
 import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
 
+//TODO remove when updating to new version
+@Deprecated("Will be removed when the next minecraft update (1.22/1.21.6) releases!", replaceWith = ReplaceWith("InventoryChangeEvent"))
 class ContainerChangeEvent(
     val item: ItemStack,
     val slot: Int,
     val screen: AbstractContainerScreen<*>,
-    val inventory: List<ItemStack>
+    val inventory: List<ItemStack>,
 ) : SkyBlockEvent() {
     val titleComponent: Component = screen.title
+    val title = titleComponent.stripped
+}
+
+class InventoryChangeEvent(
+    val item: ItemStack,
+    val slot: Slot,
+    val titleComponent: Component,
+) : SkyBlockEvent() {
+    val isInPlayerInventory = slot.container is Inventory
     val title = titleComponent.stripped
 }
