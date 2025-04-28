d3204de1c98e22071ab9bbf24f1b30f2cdfccbbc
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-04-28 16:41:41 +0200

    feat: hotm perks storage
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/impl/tagkey/ItemTag.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/impl/tagkey/ItemTag.kt
@@ -6,6 +6,7 @@ import tech.thatgravyboat.skyblockapi.api.SkyBlockAPI
 
 enum class ItemTag(path: String) : ItemTagKey {
     GLASS_PANES("glass_panes"),
+    HOTM_PERK_ITEMS("hotm_perk_items"),
     ;
 
     override val key = TagKey.create(Registries.ITEM, SkyBlockAPI.id(path))
