927bb946df76cffb6c3aa551e4be8868ff349f26
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-05-02 20:08:22 +0200

    feat: price debug command
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/impl/debug/DebugCommands.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/impl/debug/DebugCommands.kt
@@ -1,5 +1,6 @@
 package tech.thatgravyboat.skyblockapi.impl.debug
 
+import com.mojang.brigadier.arguments.StringArgumentType
 import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.ComponentSerialization
 import net.minecraft.world.item.ItemStack
@@ -7,9 +8,11 @@ import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.chat.ActionBarReceivedEvent
 import tech.thatgravyboat.skyblockapi.api.events.info.TabListHeaderFooterChangeEvent
 import tech.thatgravyboat.skyblockapi.api.events.misc.RegisterCommandsEvent
+import tech.thatgravyboat.skyblockapi.api.item.calculator.Pricing
 import tech.thatgravyboat.skyblockapi.helpers.McClient
 import tech.thatgravyboat.skyblockapi.helpers.McPlayer
 import tech.thatgravyboat.skyblockapi.modules.Module
+import tech.thatgravyboat.skyblockapi.utils.extentions.toFormattedString
 import tech.thatgravyboat.skyblockapi.utils.json.Json.toJson
 import tech.thatgravyboat.skyblockapi.utils.json.Json.toPrettyString
 import tech.thatgravyboat.skyblockapi.utils.mc.displayName
@@ -45,78 +48,92 @@ object DebugCommands {
 
     @Subscription
     fun onCommandsRegistration(event: RegisterCommandsEvent) {
-        event.register("sbapi copy") {
-            then("scoreboard") {
-                then("raw") {
+        event.register("sbapi") {
+            then("price") {
+                then("id", StringArgumentType.greedyString()) {
                     callback {
-                        copyMessage("raw scoreboard")
-                        McClient.clipboard = McClient.scoreboard.joinToString("\n") {
-                            it.toJson(ComponentSerialization.CODEC).toPrettyString()
-                        }
+                        val id = this.getArgument("id", String::class.java)
+                        val price = Pricing.getPrice(id)
+
+                        Text.of("[SkyBlockAPI] Price of $id is ${price.toFormattedString()}.") {
+                            this.color = TextColor.YELLOW
+                        }.send()
                     }
                 }
-
-                callback {
-                    copyMessage("scoreboard")
-                    McClient.clipboard = McClient.scoreboard.joinToString("\n") { it.stripped }
-                }
             }
-
-            then("tablist") {
-                then("footer") {
+            then("copy") {
+                then("scoreboard") {
                     then("raw") {
                         callback {
-                            copyMessage("raw tablist footer")
-                            McClient.clipboard = tabListFooter.toJson(ComponentSerialization.CODEC).toPrettyString()
+                            copyMessage("raw scoreboard")
+                            McClient.clipboard = McClient.scoreboard.joinToString("\n") {
+                                it.toJson(ComponentSerialization.CODEC).toPrettyString()
+                            }
                         }
                     }
 
                     callback {
-                        copyMessage("tablist footer")
-                        McClient.clipboard = tabListFooter.stripped
+                        copyMessage("scoreboard")
+                        McClient.clipboard = McClient.scoreboard.joinToString("\n") { it.stripped }
                     }
                 }
 
-                then("header") {
+                then("tablist") {
+                    then("footer") {
+                        then("raw") {
+                            callback {
+                                copyMessage("raw tablist footer")
+                                McClient.clipboard = tabListFooter.toJson(ComponentSerialization.CODEC).toPrettyString()
+                            }
+                        }
+
+                        callback {
+                            copyMessage("tablist footer")
+                            McClient.clipboard = tabListFooter.stripped
+                        }
+                    }
+
+                    then("header") {
+                        then("raw") {
+                            callback {
+                                copyMessage("raw tablist header")
+                                McClient.clipboard = tabListHeader.toJson(ComponentSerialization.CODEC).toPrettyString()
+                            }
+                        }
+
+                        callback {
+                            copyMessage("tablist header")
+                            McClient.clipboard = tabListHeader.stripped
+                        }
+                    }
+
                     then("raw") {
                         callback {
-                            copyMessage("raw tablist header")
-                            McClient.clipboard = tabListHeader.toJson(ComponentSerialization.CODEC).toPrettyString()
+                            copyMessage("raw tablist")
+                            McClient.clipboard = McClient.tablist.joinToString("\n") {
+                                it.displayName.toJson(ComponentSerialization.CODEC).toPrettyString()
+                            }
                         }
                     }
 
                     callback {
-                        copyMessage("tablist header")
-                        McClient.clipboard = tabListHeader.stripped
+                        copyMessage("tablist")
+                        McClient.clipboard = McClient.tablist.joinToString("\n") { it.displayName.stripped }
                     }
                 }
 
-                then("raw") {
+                then("item") {
                     callback {
-                        copyMessage("raw tablist")
-                        McClient.clipboard = McClient.tablist.joinToString("\n") {
-                            it.displayName.toJson(ComponentSerialization.CODEC).toPrettyString()
-                        }
+                        copyMessage("item")
+                        McClient.clipboard = McPlayer.heldItem.toJson(ItemStack.CODEC).toPrettyString()
                     }
                 }
 
-                callback {
-                    copyMessage("tablist")
-                    McClient.clipboard = McClient.tablist.joinToString("\n") { it.displayName.stripped }
-                }
-            }
-
-            then("item") {
-                callback {
-                    copyMessage("item")
-                    McClient.clipboard = McPlayer.heldItem.toJson(ItemStack.CODEC).toPrettyString()
-                }
-            }
-
-            then("actionbar") {
-                callback {
-                    copyMessage("actionbar")
-                    McClient.clipboard = actionbar
+                then("actionbar") {
+                    callback {
+                        copyMessage("actionbar")
+                        McClient.clipboard = actionbar
+                    }
                 }
             }
         }
