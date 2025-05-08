f32721fe4cb9996ac673656f8090bc342bc3bc53
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-05-08 15:09:20 +0200

    fix: backwards compat
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/area/hub/BazaarAPI.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/area/hub/BazaarAPI.kt
@@ -8,5 +8,23 @@ import tech.thatgravyboat.skyblockapi.api.remote.pricing.BazaarAPI
 object BazaarAPI {
 
     val products get() = BazaarAPI.products
-    fun getProduct(id: String?) = BazaarAPI.getProduct(id)
+    fun getProduct(id: String?): BazaarProduct? {
+        val product = BazaarAPI.getProduct(id) ?: return null
+
+        return BazaarProduct(
+            productId = product.productId,
+            sellPrice = product.sellPrice,
+            sellVolume = product.sellVolume,
+            buyPrice = product.buyPrice,
+            buyVolume = product.buyVolume,
+        )
+    }
+
+    data class BazaarProduct(
+        val productId: String,
+        val sellPrice: Double,
+        val sellVolume: Long,
+        val buyPrice: Double,
+        val buyVolume: Long,
+    )
 }
--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/area/hub/LowestBinAPI.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/area/hub/LowestBinAPI.kt
@@ -2,7 +2,6 @@ package tech.thatgravyboat.skyblockapi.api.area.hub
 
 import org.jetbrains.annotations.ApiStatus
 import tech.thatgravyboat.skyblockapi.api.remote.pricing.LowestBinAPI
-import tech.thatgravyboat.skyblockapi.api.remote.pricing.LowestBinAPI.AuctionItem
 
 @Deprecated("Moved to remote.pricing.LowestBinAPI")
 @ApiStatus.ScheduledForRemoval(inVersion = "1.21.6 or 1.22")
@@ -10,6 +9,22 @@ object LowestBinAPI {
     /** Hypixel Item Id to Prices */
     val items get() = LowestBinAPI.items
 
-    fun getPrice(id: String?): AuctionItem? = LowestBinAPI.getPrice(id)
+    fun getPrice(id: String?): AuctionItem? {
+        val item = LowestBinAPI.getPrice(id) ?: return null
+
+        return AuctionItem(
+            lowest = item.lowest,
+            highest = item.highest,
+            median = item.median,
+            mean = item.mean,
+        )
+    }
     fun getLowestPrice(id: String?): Long? = LowestBinAPI.getLowestPrice(id)
+
+    data class AuctionItem(
+        val lowest: Long,
+        val highest: Long,
+        val median: Long,
+        val mean: Double,
+    )
 }
