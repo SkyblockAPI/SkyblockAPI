8e787cae6bbf51209090b450f0dddacb87c54506
Mona <59416038+meowora@users.noreply.github.com>
2025-05-27 21:44:51 +0200

    use dispatch codecs for cost

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/remote/itemdata/Cost.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/remote/itemdata/Cost.kt
@@ -1,54 +1,44 @@
 package tech.thatgravyboat.skyblockapi.api.remote.itemdata
 
-import com.mojang.serialization.Codec
-import com.mojang.serialization.MapCodec
-import com.mojang.serialization.codecs.RecordCodecBuilder
-import me.owdding.ktcodecs.IncludedCodec
+import me.owdding.ktcodecs.FieldName
+import me.owdding.ktcodecs.GenerateCodec
+import me.owdding.ktcodecs.GenerateDispatchCodec
 import tech.thatgravyboat.skyblockapi.api.data.Essence
 import tech.thatgravyboat.skyblockapi.api.remote.pricing.Pricing
-import tech.thatgravyboat.skyblockapi.generated.KCodec
+import tech.thatgravyboat.skyblockapi.generated.DispatchHelper
+import kotlin.reflect.KClass
 
-enum class CostTypes(val codec: MapCodec<out Cost>) {
-    COINS(CoinCost.CODEC),
-    ITEM(ItemCost.CODEC),
-    ESSENCE(EssenceCost.CODEC),
+@GenerateDispatchCodec(Cost::class)
+enum class CostTypes(override val type: KClass<out Cost>) : DispatchHelper<Cost> {
+    COINS(CoinCost::class),
+    ITEM(ItemCost::class),
+    ESSENCE(EssenceCost::class),
     ;
-}
 
-data class EssenceCost(val essenceType: Essence, val amount: Int) : Cost(CostTypes.ESSENCE) {
     companion object {
-        @IncludedCodec
-        val CODEC: MapCodec<EssenceCost> = RecordCodecBuilder.mapCodec {
-            it.group(
-                KCodec.getCodec<Essence>().fieldOf("essence_type").forGetter(EssenceCost::essenceType),
-                Codec.INT.fieldOf("amount").forGetter(EssenceCost::amount),
-            ).apply(it, ::EssenceCost)
-        }
+        fun getType(id: String) = entries.first { it.id.equals(id, true) }
     }
 }
 
-data class ItemCost(val itemId: String, val amount: Int) : Cost(CostTypes.ITEM) {
-    companion object {
-        val CODEC: MapCodec<ItemCost> = RecordCodecBuilder.mapCodec {
-            it.group(
-                Codec.STRING.fieldOf("item_id").forGetter(ItemCost::itemId),
-                Codec.INT.fieldOf("amount").forGetter(ItemCost::amount),
-            ).apply(it, ::ItemCost)
-        }
-    }
-}
+@GenerateCodec
+data class EssenceCost(
+    @FieldName("essence_type") val essenceType: Essence,
+    val amount: Int,
+) : Cost(CostTypes.ESSENCE)
 
-data class CoinCost(val amount: Int) : Cost(CostTypes.COINS) {
-    companion object {
-        val CODEC: MapCodec<CoinCost> = Codec.INT.fieldOf("coins").xmap(::CoinCost, CoinCost::amount)
-    }
-}
+@GenerateCodec
+data class ItemCost(
+    @FieldName("item_id") val itemId: String,
+    val amount: Int,
+) : Cost(CostTypes.ITEM)
+
+@GenerateCodec
+data class CoinCost(
+    @FieldName("coins") val amount: Int,
+) : Cost(CostTypes.COINS)
 
 abstract class Cost(val type: CostTypes) {
     companion object {
-        @IncludedCodec
-        val CODEC: Codec<Cost> = Codec.STRING.dispatch({ it.type.name }, { CostTypes.valueOf(it).codec })
-
         fun calculateCost(cost: Cost) = when (cost) {
             is CoinCost -> cost.amount.toLong()
             is ItemCost -> cost.amount * Pricing.getPrice(cost.itemId)
