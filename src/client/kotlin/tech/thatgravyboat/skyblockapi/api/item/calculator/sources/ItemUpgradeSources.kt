3e6a65f70d1ad9ce2ea57edb45ebf21cb3fc182a
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-05-28 19:44:36 +0200

    remote.hypixel package (#109)

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/item/calculator/sources/ItemUpgradeSources.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/item/calculator/sources/ItemUpgradeSources.kt
@@ -7,9 +7,9 @@ import tech.thatgravyboat.skyblockapi.api.item.calculator.Calculator
 import tech.thatgravyboat.skyblockapi.api.item.calculator.IntDataTypeCalculator
 import tech.thatgravyboat.skyblockapi.api.remote.RepoReforgeStonesAPI
 import tech.thatgravyboat.skyblockapi.api.remote.RepoReforgeStonesAPI.getApplyCost
-import tech.thatgravyboat.skyblockapi.api.remote.itemdata.Cost
-import tech.thatgravyboat.skyblockapi.api.remote.itemdata.ItemData
-import tech.thatgravyboat.skyblockapi.api.remote.pricing.Pricing
+import tech.thatgravyboat.skyblockapi.api.remote.hypixel.itemdata.Cost
+import tech.thatgravyboat.skyblockapi.api.remote.hypixel.itemdata.ItemData
+import tech.thatgravyboat.skyblockapi.api.remote.hypixel.pricing.Pricing
 
 internal object RecombobulatorCalculator : IntDataTypeCalculator(DataTypes.RARITY_UPGRADES, "RECOMBOBULATOR_3000")
 
