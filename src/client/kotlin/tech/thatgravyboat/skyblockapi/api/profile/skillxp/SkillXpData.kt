c2b84421c3cc2cccb532e132caa73ad67d82502e
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-05-29 23:39:04 +0200

    a
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/profile/skillxp/SkillXpData.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/profile/skillxp/SkillXpData.kt
@@ -1,14 +1,14 @@
 package tech.thatgravyboat.skyblockapi.api.profile.skillxp
 
+import me.owdding.ktcodecs.GenerateCodec
 import tech.thatgravyboat.skyblockapi.api.remote.hypixel.HypixelSkillAPI
-import tech.thatgravyboat.skyblockapi.generated.KCodec
-import tech.thatgravyboat.skyblockapi.kcodec.GenerateCodec
+import tech.thatgravyboat.skyblockapi.generated.SkyblockAPICodecs
 
 @GenerateCodec
 data class SkillXpData(
     val xp: MutableMap<HypixelSkillAPI.Skill, Float> = mutableMapOf(),
 ) {
     companion object {
-        internal val CODEC = KCodec.getCodec<SkillXpData>()
+        internal val CODEC = SkyblockAPICodecs.getCodec<SkillXpData>()
     }
 }
