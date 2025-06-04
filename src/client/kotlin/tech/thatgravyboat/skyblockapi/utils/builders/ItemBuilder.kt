b1a0d5b54703161d886507f5f63a86461299e198
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-06-04 12:44:45 +0200

    feat: visual replace prefix/suffix
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/utils/builders/ItemBuilder.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/utils/builders/ItemBuilder.kt
@@ -1,14 +1,26 @@
 package tech.thatgravyboat.skyblockapi.utils.builders
 
+import me.owdding.ktmodules.Module
 import net.minecraft.core.component.DataComponentPatch
 import net.minecraft.core.component.DataComponentType
 import net.minecraft.core.component.DataComponents
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.MutableComponent
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
 import net.minecraft.world.item.component.ItemLore
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.base.predicates.InventoryTitle
+import tech.thatgravyboat.skyblockapi.api.events.screen.InventoryChangeEvent
 import tech.thatgravyboat.skyblockapi.api.item.asVisualItemAccessor
+import tech.thatgravyboat.skyblockapi.api.item.replaceVisually
+import tech.thatgravyboat.skyblockapi.utils.extentions.getRawLore
 import tech.thatgravyboat.skyblockapi.utils.extentions.holder
+import tech.thatgravyboat.skyblockapi.utils.extentions.toIntValue
+import tech.thatgravyboat.skyblockapi.utils.regex.RegexUtils.anyMatch
+import tech.thatgravyboat.skyblockapi.utils.text.CommonText
+import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.style
 import kotlin.jvm.optionals.getOrNull
 
@@ -43,17 +55,42 @@ class ItemBuilder {
         }
     }
 
+    fun namePrefix(prefix: String) = namePrefix(Component.literal(prefix))
+    fun namePrefix(prefix: Component) = apply {
+        components.set(
+            DataComponents.CUSTOM_NAME,
+            Text.join(
+                prefix,
+                components.build().get(DataComponents.CUSTOM_NAME)?.getOrNull()?.copy()?.setItalic() ?: CommonText.EMPTY,
+            ),
+        )
+    }
+
+
     fun name(name: String) = name(Component.literal(name))
     fun name(name: Component) = apply {
         components.set(
             DataComponents.CUSTOM_NAME,
-            name.copy().style {
-                if (!this.isItalic) {
-                    return@style this.withItalic(false)
-                }
-                this
-            },
+            name.copy().setItalic(),
         )
+    }
+
+    fun nameSuffix(suffix: String) = nameSuffix(Component.literal(suffix))
+    fun nameSuffix(suffix: Component) = apply {
+        components.set(
+            DataComponents.CUSTOM_NAME,
+            Text.join(
+                components.build().get(DataComponents.CUSTOM_NAME)?.getOrNull()?.copy()?.setItalic() ?: CommonText.EMPTY,
+                suffix,
+            ),
+        )
+    }
+
+    private fun MutableComponent.setItalic() = style {
+        if (!this.isItalic) {
+            return@style this.withItalic(false)
+        }
+        this
     }
 
     fun tooltip(init: TooltipBuilder.() -> Unit) = apply {
@@ -80,6 +117,26 @@ class ItemBuilder {
                 it.`skyblockapi$setSlotText`(customSlotText)
                 it.`skyblockapi$setOnClickAction`(clickAction)
                 it.`skyblockapi$setBackgroundItem`(backgroundItem)
+            }
+        }
+    }
+}
+
+@Module
+object Pest {
+    val regex = "ൠ This plot has (?<amount>.*) Pests?!".toRegex()
+
+    @Subscription
+    @InventoryTitle("Configure Plots")
+    fun onInv(event: InventoryChangeEvent) {
+        regex.anyMatch(event.item.getRawLore(), "amount") { (amount) ->
+            val amount = amount.toIntValue().takeUnless { it == 0 } ?: return@anyMatch
+            event.item.replaceVisually {
+                copyFrom(event.item)
+                namePrefix("testing")
+                nameSuffix("aaaaaa")
+                backgroundItem = Items.LIME_STAINED_GLASS_PANE.defaultInstance
+                customSlotText = "$amount"
             }
         }
     }
