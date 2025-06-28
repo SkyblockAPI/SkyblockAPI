bd08d505520e5128e8edb93fa7dfe59c150662ca
HiZe <superhize@gmail.com>
2025-06-28 10:15:09 +0200

    feat: absorb logs (#155)
    Co-authored-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/datatype/DataTypes.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/datatype/DataTypes.kt
@@ -53,6 +53,8 @@ object DataTypes {
     val APPLIED_DYE: DataType<String> = GenericDataTypes.APPLIED_DYE
     val PET_DATA: DataType<PetData> = GenericDataTypes.PET_DATA
     val SNOWBALLS: DataType<Pair<Int, Int>> = LoreDataTypes.SNOWBALLS
+    val ABSORB_LOGS: DataType<Long> = GenericDataTypes.ABSORB_LOGS
+    val LOGS_CUT: DataType<Long> = GenericDataTypes.LOGS_CUT
 
     // Aging Items
     val SECONDS_HELD: DataType<Int> = GenericDataTypes.SECONDS_HELD
