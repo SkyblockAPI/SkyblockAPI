264c720085fcb3cc3f4febc2b17b931bf84960f0
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-07-11 22:46:58 +0200

    refactor: sophie dm
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/profile/items/forge/ForgeAPI.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/profile/items/forge/ForgeAPI.kt
@@ -24,10 +24,7 @@ object ForgeAPI {
         if (event.isSkyBlockFiller) return
 
         val index = event.slot.index - 9
-        val id = event.item.getSkyBlockId() ?: run {
-            ForgeStorage.clearSlot(index)
-            return
-        }
+        val id = event.item.getSkyBlockId() ?: return ForgeStorage.clearSlot(index)
 
         durationRegex.anyMatch(event.item.getRawLore(), "duration") { (duration) ->
             val duration = duration.parseDuration() ?: return@anyMatch
