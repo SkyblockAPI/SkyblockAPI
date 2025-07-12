96889b9a58afedc206ab679a5f07c057e27b9ced
Gravy Boat <gravy@thatgravyboat.tech>
2025-07-12 19:32:31 -0230

    rollback publishing changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -206,7 +206,12 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             artifactId = "skyblock-api"
-            from(components["java"])
+
+            artifact(tasks["1215JarInJar"])
+            artifact(tasks["1217JarInJar"])
+            artifact(tasks["generateMetadataFileForMavenPublication"].outputs.files.first()) {
+                extension = "module"
+            }
 
             pom {
                 name.set("SkyblockAPI")
