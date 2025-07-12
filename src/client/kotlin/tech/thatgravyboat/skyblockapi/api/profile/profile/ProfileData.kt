8bd2df6225fabfcbe1929299dcfb429f42dfba83
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-07-12 16:51:58 +0200

    feat: bingo rank
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skyblockapi/api/profile/profile/ProfileData.kt
+++ src/client/kotlin/tech/thatgravyboat/skyblockapi/api/profile/profile/ProfileData.kt
@@ -2,6 +2,7 @@ package tech.thatgravyboat.skyblockapi.api.profile.profile
 
 import com.mojang.serialization.Codec
 import me.owdding.ktcodecs.GenerateCodec
+import tech.thatgravyboat.skyblockapi.api.data.SkyBlockRarity
 import tech.thatgravyboat.skyblockapi.generated.KCodec
 
 @GenerateCodec
@@ -10,6 +11,7 @@ data class ProfileData(
     val sbLevel: MutableMap<String, Int> = mutableMapOf(),
     val sbLevelProgress: MutableMap<String, Int> = mutableMapOf(),
     val coop: MutableMap<String, Boolean> = mutableMapOf(),
+    var bingoRank: SkyBlockRarity?,
 ) {
     companion object {
         val CODEC: Codec<ProfileData> = KCodec.getCodec<ProfileData>()
