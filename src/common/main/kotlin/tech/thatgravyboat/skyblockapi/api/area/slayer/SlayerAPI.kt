60ffd45f1fac20521c468b6b91ffd5e795c473b2
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-07-22 20:43:09 +0200

    nvm..?
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/area/slayer/SlayerAPI.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/area/slayer/SlayerAPI.kt
@@ -75,7 +75,7 @@ object SlayerAPI {
             reset()
         } else if (type == null && level == 0) {
             val index = event.added.indexOfFirst { slayerQuestRegex.matches(it) }
-            if (index != -1 && event.added.size > index) {
+            if (index != -1 && event.new.size > index) {
                 slayerTypeRegex.match(event.added[index + 1], "type", "level") { (type, level) ->
                     SlayerAPI.type = SlayerType.fromDisplayName(type)
                     SlayerAPI.level = level.parseRomanNumeral()
