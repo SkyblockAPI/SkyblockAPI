8a9f4815ea66e81d82184fd3a5c3b660b9bbfae4
Mona <59416038+meowora@users.noreply.github.com>
2025-07-25 23:17:48 +0200

    fix: debug time

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/profile/items/sacks/SacksAPI.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/profile/items/sacks/SacksAPI.kt
@@ -102,7 +102,7 @@ object SacksAPI {
             if (amount == previousAmount) {
                 return@forEach
             }
-            val isInvalid = entry?.lastUpdated?.since()?.let { it < 1.minutes } == true
+            val isInvalid = entry?.lastUpdated?.since()?.let { it < 15.minutes } == true
             if (isInvalid) {
                 return@forEach
             }
