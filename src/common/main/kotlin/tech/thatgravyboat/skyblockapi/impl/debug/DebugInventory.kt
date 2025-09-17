03a9379570727bcac2f952dec0e5d4b1c259b447
Mona <59416038+meowora@users.noreply.github.com>
2025-09-17 18:51:46 +0200

    .

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/impl/debug/DebugInventory.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/impl/debug/DebugInventory.kt
@@ -148,7 +148,7 @@ internal object DebugInventory {
         ;
 
         val title = name.toTitleCase()
-        val keyName: Component = InputConstants.Type.KEYSYM.getOrCreate(key).displayName;
+        val keyName: Component = InputConstants.Type.KEYSYM.getOrCreate(key).displayName
 
         fun initCopy(slot: Slot): Boolean {
             val data = copy(slot) ?: return false
