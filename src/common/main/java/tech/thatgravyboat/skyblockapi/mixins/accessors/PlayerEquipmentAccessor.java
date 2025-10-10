1fd2a3908afce2149bcf213f623a6141da020cfa
Mona <59416038+meowora@users.noreply.github.com>
2025-10-10 16:44:04 +0200

    fix: player inventory events not being fired anymore (#229)

---
+++ src/common/main/java/tech/thatgravyboat/skyblockapi/mixins/accessors/PlayerEquipmentAccessor.java
@@ -0,0 +1,14 @@
+package tech.thatgravyboat.skyblockapi.mixins.accessors;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.PlayerEquipment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(PlayerEquipment.class)
+public interface PlayerEquipmentAccessor {
+
+    @Accessor("player")
+    Player skyblockapi$player();
+
+}
