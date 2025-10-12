a8a120973a1d6ae6a8ae219420a6b3f0f6960bf3
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-10-12 17:58:51 +0200

    refactor: move tabwidget regex into enum
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/events/info/TabWidgetChangeEvent.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/events/info/TabWidgetChangeEvent.kt
@@ -1,10 +1,12 @@
 package tech.thatgravyboat.skyblockapi.api.events.info
 
 import net.minecraft.network.chat.Component
+import org.intellij.lang.annotations.Language
 import tech.thatgravyboat.skyblockapi.api.events.base.EventBus
 import tech.thatgravyboat.skyblockapi.api.events.base.SkyBlockEvent
 import tech.thatgravyboat.skyblockapi.impl.events.TabListEventHandler
 import tech.thatgravyboat.skyblockapi.utils.extentions.toFormattedName
+import tech.thatgravyboat.skyblockapi.utils.regex.RegexGroup
 
 data class TabWidgetChangeEvent(
     val widget: TabWidget,
@@ -13,55 +15,57 @@ data class TabWidgetChangeEvent(
     val newComponents: List<Component>,
 ) : SkyBlockEvent() {
 
-    override fun post(bus: EventBus): Boolean =
-        bus.post(this, this.widget)
+    override fun post(bus: EventBus): Boolean = bus.post(this, this.widget)
 }
 
-enum class TabWidget {
-    AREA,
-    PROFILE,
-    PET,
-    DAILY_QUESTS,
-    FORGES,
-    COMMISSIONS,
-    SKILLS,
-    POWDERS,
-    ELECTION,
-    CRYSTALS,
-    BESTIARY,
-    COLLECTION,
-    STATS,
-    EVENT,
-    PARTY,
-    MINIONS,
-    DUNGEONS,
-    ESSENCE,
-    GOOD_TO_KNOW,
-    SHEN,
-    ADVERTISEMENT,
-    TRAPPER,
-    EVENT_TRACKERS,
-    FROZEN_CORPSES,
-    ACTIVE_EFFECTS,
-    MINING_EVENT,
-    TIMERS,
-    COMPOSTER,
-    JACOBS_CONTEST,
-    PESTS,
-    PEST_TRAPS,
-    VISITORS,
-    RNG_METER,
-    DOWNED,
-    TEAM_DEATHS,
-    DISCOVERIES,
-    PUZZLES,
-    REPUTATION,
-    TROPHY_FISH,
-    FACTION_QUESTS,
-    FOREST_WHISPERS,
-    MOONGLADE_BEACON,
-    FIRE_SALE,
+@Suppress("unused")
+enum class TabWidget(@Language("RegExp") regex: String) {
+    AREA("(?:Area|Dungeon): (?<area>.*)"),
+    PROFILE("Profile: (?<profile>.*)"),
+    PET("Pet:"),
+    DAILY_QUESTS("Daily Quests:"),
+    FORGES("Forges:(?: \\((?<active>[\\d,.]+)/(?<max>[\\d,.]+)\\))?"),
+    COMMISSIONS("Commissions:"),
+    SKILLS("Skills:(?: (?<avg>[\\d.]+))?"),
+    POWDERS("Powders:"),
+    ELECTION("Election: (?<election>.*)"),
+    CRYSTALS("Crystals:"),
+    BESTIARY("Bestiary:"),
+    COLLECTION("Collection:"),
+    STATS("Stats:"),
+    EVENT("Event: (?<event>.*)"),
+    PARTY("Party: (?<party>.*)"),
+    MINIONS("Minions: (?<party>.*)"), // Party
+    DUNGEONS("Dungeons:"),
+    ESSENCE("Essence:"),
+    GOOD_TO_KNOW("Good to know:"),
+    SHEN("Shen: \\((?<duration>[\\ddmsh,]+)\\)"),
+    ADVERTISEMENT("Advertisement:"),
+    TRAPPER("Trapper:"),
+    EVENT_TRACKERS("Event Trackers:"),
+    FROZEN_CORPSES("Frozen Corpses:"),
+    ACTIVE_EFFECTS("Active Effects:(?: \\((?<amount>\\d+)\\))?"),
+    MINING_EVENT("Mining Event: (?<event>.*)"),
+    TIMERS("Timers:"),
+    COMPOSTER("Composter:"),
+    JACOBS_CONTEST("Jacob's Contest:(?: (?<time>.*))?"),
+    PESTS("Pests:(?: (?<amount>\\d+))?"),
+    PEST_TRAPS("Pest Traps: (?<amount>[\\d,.]+)/(?<max>[\\d,.]+)"),
+    VISITORS("Visitors: \\((?<amount>\\d+)\\)"),
+    RNG_METER("RNG Meter"),
+    DOWNED("Downed: (?<status>.*)"),
+    TEAM_DEATHS("Team Deaths: (?<amount>\\d+)"),
+    DISCOVERIES("Discoveries: (?<amount>\\d+)"),
+    PUZZLES("Puzzles: \\((?<amount>\\d+)\\)"),
+    REPUTATION("(?:Mage|Barbarian) Reputation:"),
+    TROPHY_FISH("Trophy Fish:"),
+    FACTION_QUESTS("Faction Quests:"),
+    FOREST_WHISPERS("Forest Whispers: (?<amount>[\\dkmb,.]+)"),
+    MOONGLADE_BEACON("Moonglade Beacon: (?<amount>[\\d,.]+) Stacks?"),
+    FIRE_SALE("Fire Sales: \\((?<amount>[\\d,.]+)\\)"),
     ;
+
+    val regex = RegexGroup.TABLIST_WIDGET.create(name.lowercase(), regex)
 
     val currentLines: List<String>
         get() = TabListEventHandler.widgets[this].orEmpty()
