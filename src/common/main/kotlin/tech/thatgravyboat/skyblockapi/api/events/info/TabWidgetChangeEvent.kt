eb76451c80e3d476f5d2147bc0244913d653fe6f
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-10-12 18:46:28 +0200

    reorder
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/events/info/TabWidgetChangeEvent.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/events/info/TabWidgetChangeEvent.kt
@@ -20,50 +20,63 @@ data class TabWidgetChangeEvent(
 
 @Suppress("unused")
 enum class TabWidget(@Language("RegExp") regex: String) {
+    // General
     AREA("(?:Area|Dungeon): (?<area>.*)"),
     PROFILE("Profile: (?<profile>.*)"),
     PET("Pet:"),
     DAILY_QUESTS("Daily Quests:"),
-    FORGES("Forges:(?: \\((?<active>[\\d,.]+)/(?<max>[\\d,.]+)\\))?"),
-    COMMISSIONS("Commissions:"),
     SKILLS("Skills:(?: (?<avg>[\\d.]+))?"),
-    POWDERS("Powders:"),
     ELECTION("Election: (?<election>.*)"),
-    CRYSTALS("Crystals:"),
     BESTIARY("Bestiary:"),
     COLLECTION("Collection:"),
     STATS("Stats:"),
     EVENT("Event: (?<event>.*)"),
-    PARTY("Party: (?<party>.*)"),
-    MINIONS("Minions: (?<amount>.*)"),
-    DUNGEONS("Dungeons:"),
-    ESSENCE("Essence:"),
-    GOOD_TO_KNOW("Good to know:"),
-    SHEN("Shen: \\((?<duration>[\\ddmsh,]+)\\)"),
-    ADVERTISEMENT("Advertisement:"),
-    TRAPPER("Trapper:"),
     EVENT_TRACKERS("Event Trackers:"),
-    FROZEN_CORPSES("Frozen Corpses:"),
     ACTIVE_EFFECTS("Active Effects:(?: \\((?<amount>\\d+)\\))?"),
-    MINING_EVENT("Mining Event: (?<event>.*)"),
     TIMERS("Timers:"),
+    FIRE_SALE("Fire Sales: \\((?<amount>[\\d,.]+)\\)"),
+    MINIONS("Minions: (?<amount>.*)"),
+
+    // Mining
+    FORGES("Forges:(?: \\((?<active>[\\d,.]+)/(?<max>[\\d,.]+)\\))?"),
+    COMMISSIONS("Commissions:"),
+    POWDERS("Powders:"),
+    CRYSTALS("Crystals:"),
+    MINING_EVENT("Mining Event: (?<event>.*)"),
+    FROZEN_CORPSES("Frozen Corpses:"),
+
+    // Foraging
+    STARBORN_TEMPLE("Starborn Temple:"),
+    FOREST_WHISPERS("Forest Whispers: (?<amount>[\\dkmb,.]+)"),
+    MOONGLADE_BEACON("Moonglade Beacon: (?<amount>[\\d,.]+) Stacks?"),
+
+    // Garden + Farming
     COMPOSTER("Composter:"),
     JACOBS_CONTEST("Jacob's Contest:(?: (?<time>.*))?"),
     PESTS("Pests:(?: (?<amount>\\d+))?"),
     PEST_TRAPS("Pest Traps: (?<amount>[\\d,.]+)/(?<max>[\\d,.]+)"),
     VISITORS("Visitors: \\((?<amount>\\d+)\\)"),
-    RNG_METER("RNG Meter"),
+    TRAPPER("Trapper:"),
+
+    // Crimson Isle
+    REPUTATION("(?:Mage|Barbarian) Reputation:"),
+    TROPHY_FISH("Trophy Fish:"),
+    FACTION_QUESTS("Faction Quests:"),
+
+    // Dungeons + Dungeon Hub
     DOWNED("Downed: (?<status>.*)"),
     TEAM_DEATHS("Team Deaths: (?<amount>\\d+)"),
     DISCOVERIES("Discoveries: (?<amount>\\d+)"),
     PUZZLES("Puzzles: \\((?<amount>\\d+)\\)"),
-    REPUTATION("(?:Mage|Barbarian) Reputation:"),
-    TROPHY_FISH("Trophy Fish:"),
-    FACTION_QUESTS("Faction Quests:"),
-    FOREST_WHISPERS("Forest Whispers: (?<amount>[\\dkmb,.]+)"),
-    MOONGLADE_BEACON("Moonglade Beacon: (?<amount>[\\d,.]+) Stacks?"),
-    FIRE_SALE("Fire Sales: \\((?<amount>[\\d,.]+)\\)"),
-    STARBORN_TEMPLE("Starborn Temple:"),
+    RNG_METER("RNG Meter"),
+    PARTY("Party: (?<party>.*)"),
+    DUNGEONS("Dungeons:"),
+    ESSENCE("Essence:"),
+
+    // Rift
+    GOOD_TO_KNOW("Good to know:"),
+    SHEN("Shen: \\((?<duration>[\\ddmsh,]+)\\)"),
+    ADVERTISEMENT("Advertisement:"),
     ;
 
     val regex = RegexGroup.TABLIST_WIDGET.create(name.lowercase(), regex)
