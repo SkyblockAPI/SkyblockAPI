f272d01488c027b1981d9b1277977afffed3aec1
Empa <itsempa@users.noreply.github.com>
2025-10-17 20:43:16 +0200

    feat: add more stuff in dungeon api

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/area/dungeon/DungeonPlayer.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/area/dungeon/DungeonPlayer.kt
@@ -1,5 +1,10 @@
 package tech.thatgravyboat.skyblockapi.api.area.dungeon
 
+import net.minecraft.world.entity.player.Player
+import tech.thatgravyboat.skyblockapi.helpers.McLevel
+import tech.thatgravyboat.skyblockapi.utils.extentions.isRealPlayer
+import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
+
 class DungeonPlayer(
     val name: String,
     dungeonClass: DungeonClass?,
@@ -12,6 +17,9 @@ class DungeonPlayer(
         internal set
     var classLevel: Int? = classLevel
         internal set
+
+    val realPlayer: Player?
+        get() = McLevel.self.players().find { it.isRealPlayer() && it.name.stripped == name }
 
     internal fun missingData(): Boolean = dungeonClass == null || classLevel == null
 }
