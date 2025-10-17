cd9d2a60f7f583fcf6f4ac11745bd46e3beb8c4a
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-10-17 20:12:18 +0200

    fix: new mineshaft variants (#243)

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/events/location/mineshaft/MineshaftEnteredEvent.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/events/location/mineshaft/MineshaftEnteredEvent.kt
@@ -1,6 +1,9 @@
 package tech.thatgravyboat.skyblockapi.api.events.location.mineshaft
 
 import tech.thatgravyboat.skyblockapi.api.area.mining.mineshaft.MineshaftType
+import tech.thatgravyboat.skyblockapi.api.area.mining.mineshaft.MineshaftVariant
 import tech.thatgravyboat.skyblockapi.api.events.base.SkyBlockEvent
 
-data class MineshaftEnteredEvent(val type: MineshaftType?, val isCrystal: Boolean) : SkyBlockEvent()
+data class MineshaftEnteredEvent(val type: MineshaftType?, val variant: MineshaftVariant?) : SkyBlockEvent() {
+    val isCrystal: Boolean = variant == MineshaftVariant.CRYSTAL
+}
