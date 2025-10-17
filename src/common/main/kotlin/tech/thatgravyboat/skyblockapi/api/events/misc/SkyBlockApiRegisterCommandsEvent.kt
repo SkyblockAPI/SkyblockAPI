39e2643028411b70a9879617ef817c35e958a8c2
Empa <itsempa@users.noreply.github.com>
2025-10-17 20:45:18 +0200

    fix: actually remove sbapi command :3

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/events/misc/SkyBlockApiRegisterCommandsEvent.kt
+++
@@ -1,5 +0,0 @@
-package tech.thatgravyboat.skyblockapi.api.events.misc
-
-class SkyBlockApiRegisterCommandsEvent(
-    baseEvent: RegisterCommandsEvent,
-) : AbstractModRegisterCommandsEvent(baseEvent, "sbapi")
