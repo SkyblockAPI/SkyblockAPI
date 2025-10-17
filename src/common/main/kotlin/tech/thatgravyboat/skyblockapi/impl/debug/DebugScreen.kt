9c6f3fb498994b0a61f1a041edb8678d29c22db5
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-10-17 21:07:25 +0200

    missing stub
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/impl/debug/DebugScreen.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/impl/debug/DebugScreen.kt
@@ -3,11 +3,13 @@ package tech.thatgravyboat.skyblockapi.impl.debug
 import net.minecraft.client.gui.components.AbstractWidget
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.network.chat.Component
+import net.msrandom.stub.Stub
 import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
 import kotlin.time.Instant
 
+@Stub
 internal expect fun <T> DebugScreen(
     title: String,
     messages: List<Pair<Instant, T>>,
