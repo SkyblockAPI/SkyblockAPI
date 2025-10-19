2a31caa04f839ae15c069c1e84bd955b6733e02d
Empa <42304516+ItsEmpa@users.noreply.github.com>
2025-10-19 20:34:12 +0200

    feat: add more stuff in dungeon api (#246)
    Co-authored-by: Empa <itsempa@users.noreply.github.com>

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/utils/extentions/EntityExtensions.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/utils/extentions/EntityExtensions.kt
@@ -3,6 +3,7 @@ package tech.thatgravyboat.skyblockapi.utils.extentions
 import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.ai.attributes.Attributes
+import net.minecraft.world.entity.player.Player
 import tech.thatgravyboat.skyblockapi.hooks.AttributeInstanceHook
 import tech.thatgravyboat.skyblockapi.hooks.DataItemHook
 import tech.thatgravyboat.skyblockapi.mixins.accessors.LivingEntityAccessor
@@ -12,6 +13,8 @@ fun LivingEntity.getHelmet() = getItemBySlot(EquipmentSlot.HEAD)
 fun LivingEntity.getChestplate() = getItemBySlot(EquipmentSlot.CHEST)
 fun LivingEntity.getLeggings() = getItemBySlot(EquipmentSlot.LEGS)
 fun LivingEntity.getBoots() = getItemBySlot(EquipmentSlot.FEET)
+
+fun Player.isRealPlayer() = uuid.version() == 4
 
 fun LivingEntity.getArmor() = listOf(getHelmet(), getChestplate(), getLeggings(), getBoots())
 
