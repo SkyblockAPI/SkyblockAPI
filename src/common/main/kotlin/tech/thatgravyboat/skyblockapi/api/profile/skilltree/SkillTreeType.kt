e5b442777147aade973444284725409574e11175
Empa <itsempa@users.noreply.github.com>
2025-10-28 22:38:26 +0100

    fix: delete reflection (sophie big meanie)

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/profile/skilltree/SkillTreeType.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/profile/skilltree/SkillTreeType.kt
@@ -2,13 +2,12 @@ package tech.thatgravyboat.skyblockapi.api.profile.skilltree
 
 import tech.thatgravyboat.skyblockapi.api.profile.hotf.HotfAPI
 import tech.thatgravyboat.skyblockapi.api.profile.hotm.HotmAPI
-import tech.thatgravyboat.skyblockapi.utils.extentions.getSealedObjects
 
 sealed class SkillTreeType<out API : SkillTreeAPI<*, *, *>>(val api: API) {
     object Hotm : SkillTreeType<HotmAPI>(HotmAPI)
     object Hotf : SkillTreeType<HotfAPI>(HotfAPI)
 
     companion object {
-        val types: List<SkillTreeType<*>> = getSealedObjects()
+        val types: List<SkillTreeType<*>> = listOf(Hotm, Hotf)
     }
 }
