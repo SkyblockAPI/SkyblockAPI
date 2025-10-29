d06c3d7bbca4b1f1bc3f3834a5472324d09263f5
Gravy Boat <gravy@thatgravyboat.tech>
2025-10-29 06:24:11 -0230

    fix: self referential skill tree

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/profile/skilltree/SkillTreeType.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/profile/skilltree/SkillTreeType.kt
@@ -3,9 +3,12 @@ package tech.thatgravyboat.skyblockapi.api.profile.skilltree
 import tech.thatgravyboat.skyblockapi.api.profile.hotf.HotfAPI
 import tech.thatgravyboat.skyblockapi.api.profile.hotm.HotmAPI
 
-sealed class SkillTreeType<out API : SkillTreeAPI<*, *, *>>(val api: API) {
-    object Hotm : SkillTreeType<HotmAPI>(HotmAPI)
-    object Hotf : SkillTreeType<HotfAPI>(HotfAPI)
+sealed class SkillTreeType<out API : SkillTreeAPI<*, *, *>>(api: () -> API) {
+
+    val api: API by lazy(api)
+
+    object Hotm : SkillTreeType<HotmAPI>({ HotmAPI })
+    object Hotf : SkillTreeType<HotfAPI>({ HotfAPI })
 
     companion object {
         val types: List<SkillTreeType<*>> = listOf(Hotm, Hotf)
