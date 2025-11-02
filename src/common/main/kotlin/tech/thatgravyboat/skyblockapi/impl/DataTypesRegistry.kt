e49f74f8a4e2dc8cad1256791683bdf5af3bdd8f
Mona <59416038+meowora@users.noreply.github.com>
2025-11-02 17:52:33 +0100

    Add backing property and expose immutable list

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/impl/DataTypesRegistry.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/impl/DataTypesRegistry.kt
@@ -10,21 +10,22 @@ import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
 
 object DataTypesRegistry {
 
-    val types: MutableList<DataType<*>> = mutableListOf()
+    private val _types: MutableList<DataType<*>> = mutableListOf()
+    val types: List<DataType<*>> get() = _types
 
     internal fun load() {
-        RegisterDataTypesEvent(types::add).post(SkyBlockAPI.eventBus)
+        RegisterDataTypesEvent(_types::add).post(SkyBlockAPI.eventBus)
     }
 
     internal fun addDataType(dataType: DataType<*>) {
-        if (dataType !in types) types.add(dataType)
+        if (dataType !in _types) _types.add(dataType)
     }
 
     @ApiStatus.Internal
     fun getData(item: ItemStack) = getDataImpl(item)
 
     internal fun getDataImpl(item: ItemStack): Map<DataType<*>, *> = runCatching {
-        types
+        _types
             .associateWith { it.factory(item) }
             .filterValues { if (it is Map<*, *>) it.isNotEmpty() else true }
             .filterValues { if (it is Collection<*>) it.isNotEmpty() else true }
