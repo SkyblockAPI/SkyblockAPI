ee035efeb49fbec41630dc73684ddcae54ef79c6
Gravy Boat <gravy@thatgravyboat.tech>
2025-11-09 05:43:04 -0330

    fix: post TabListHeaderFooterChangeEvent on main thread

--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/impl/events/TabListEventHandler.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/impl/events/TabListEventHandler.kt
@@ -165,14 +165,14 @@ internal object TabListEventHandler {
     @Subscription
     fun onPacketReceived(event: PacketReceivedEvent) {
         if (event.packet is ClientboundTabListPacket) {
-            TabListHeaderFooterChangeEvent(
-                footer,
-                header,
-                event.packet.footer(),
-                event.packet.header(),
-            ).post(SkyBlockAPI.eventBus)
+            val oldHeader = this.header
+            val oldFooter = this.footer
             this.header = event.packet.header()
             this.footer = event.packet.footer()
+
+            McClient.runNextTick {
+                TabListHeaderFooterChangeEvent(oldFooter, oldHeader, this.footer, this.header).post(SkyBlockAPI.eventBus)
+            }
         }
     }
 }
