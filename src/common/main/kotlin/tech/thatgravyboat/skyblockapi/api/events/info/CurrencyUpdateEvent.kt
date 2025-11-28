6f0029c7a3be182d3f73052679edf21ef754cbe1
Empa <itsempa@users.noreply.github.com>
2025-11-28 10:09:10 +0100

    feat: add CurrencyUpdateEvent, and use it for updating bits available

---
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/events/info/CurrencyUpdateEvent.kt
@@ -0,0 +1,26 @@
+package tech.thatgravyboat.skyblockapi.api.events.info
+
+import tech.thatgravyboat.skyblockapi.api.events.base.SkyBlockEvent
+
+sealed class CurrencyUpdateEvent<N : Number>(val new: N, val old: N) : SkyBlockEvent() {
+
+    class Purse(new: Double, old: Double) : CurrencyUpdateEvent<Double>(new, old)
+    class Bank(new: Long, old: Long) : CurrencyUpdateEvent<Long>(new, old)
+    class CoopBank(new: Long, old: Long) : CurrencyUpdateEvent<Long>(new, old)
+    class Bits(new: Long, old: Long) : CurrencyUpdateEvent<Long>(new, old)
+    class Motes(new: Long, old: Long) : CurrencyUpdateEvent<Long>(new, old)
+    class Copper(new: Long, old: Long) : CurrencyUpdateEvent<Long>(new, old)
+    class NorthStars(new: Long, old: Long) : CurrencyUpdateEvent<Long>(new, old)
+    class Gems(new: Long, old: Long) : CurrencyUpdateEvent<Long>(new, old)
+
+    companion object {
+        @get:JvmName("diffLong")
+        val CurrencyUpdateEvent<Long>.diff get() = new - old
+        @get:JvmName("diffDouble")
+        val CurrencyUpdateEvent<Double>.diff get() = new - old
+
+    }
+
+
+
+}
--- src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/profile/currency/CurrencyAPI.kt
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/profile/currency/CurrencyAPI.kt
@@ -1,15 +1,16 @@
 package tech.thatgravyboat.skyblockapi.api.profile.currency
 
 import me.owdding.ktmodules.Module
+import tech.thatgravyboat.skyblockapi.api.data.stored.CommunityCenterStorage
 import tech.thatgravyboat.skyblockapi.api.data.stored.CurrencyStorage
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyOnSkyBlock
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyWidget
+import tech.thatgravyboat.skyblockapi.api.events.info.CurrencyUpdateEvent
 import tech.thatgravyboat.skyblockapi.api.events.info.ScoreboardUpdateEvent
 import tech.thatgravyboat.skyblockapi.api.events.info.TabWidget
 import tech.thatgravyboat.skyblockapi.api.events.info.TabWidgetChangeEvent
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
-import tech.thatgravyboat.skyblockapi.api.profile.community.CommunityCenterAPI
 import tech.thatgravyboat.skyblockapi.utils.extentions.parseFormattedDouble
 import tech.thatgravyboat.skyblockapi.utils.extentions.parseFormattedLong
 import tech.thatgravyboat.skyblockapi.utils.regex.RegexGroup
@@ -20,7 +21,14 @@ enum class PurseType {
     UNKNOWN,
     NORMAL,
     PIGGY,
+    ;
+
+    companion object {
+        fun fromName(name: String): PurseType = entries.find { it.name.equals(name, true) } ?: UNKNOWN
+    }
 }
+
+private typealias Event = CurrencyUpdateEvent<*>
 
 @Module
 @Suppress("MemberVisibilityCanBePrivate")
@@ -33,6 +41,7 @@ object CurrencyAPI {
         "(?i) Bank: (?<coop>\\.\\.\\.|[\\d,.kmb]+) / (?<personal>[\\d,.kmb]+)",
     )
     private val soulflowRegex = widgetGroup.create("profile.soulflow", "(?i) Soulflow: (?<soulflow>[\\d,.kmb]+)")
+    private val gemsRegex = widgetGroup.create("gems", "(?i) Gems: (?<gems>[\\d,.kmb]+)")
 
     private val currencyGroup = RegexGroup.SCOREBOARD.group("currency")
     private val purseRegex = currencyGroup.create("purse", "^(?<type>Purse|Piggy): (?<purse>[\\d,.kmb]+)")
@@ -67,25 +76,36 @@ object CurrencyAPI {
     var northStars: Long by CurrencyStorage::northStars
         private set
 
+    var gems: Long by CommunityCenterStorage::gems
+        private set
+
     // TODO: move somewhere else, since soulflow isn't really a currency
     var soulflow: Long by CurrencyStorage::soulflow
         private set
 
-    val gems: Long by CommunityCenterAPI::gems
-
     @Subscription
-    @OnlyWidget(TabWidget.PROFILE)
+    @OnlyWidget(TabWidget.PROFILE, TabWidget.AREA)
     fun onTabListWidgetChange(event: TabWidgetChangeEvent) {
-        bankSingleRegex.anyMatch(event.new, "bank") { (bank) ->
-            this.coopBank = bank.parseFormattedLong()
-            this.personalBank = 0
-        }
-        bankCoopRegex.anyMatch(event.new, "coop", "personal") { (coop, personal) ->
-            this.coopBank = coop.parseFormattedLong()
-            this.personalBank = personal.parseFormattedLong()
-        }
-        soulflowRegex.anyMatch(event.new, "soulflow") { (soulflow) ->
-            this.soulflow = soulflow.parseFormattedLong()
+        when (event.widget) {
+            TabWidget.PROFILE -> {
+                bankSingleRegex.anyMatch(event.new, "bank") { (bank) ->
+                    this.coopBank = post(bank, this.coopBank, Event::CoopBank)
+                    this.personalBank = post(0, this.personalBank, Event::Bank)
+                }
+                bankCoopRegex.anyMatch(event.new, "coop", "personal") { (coop, personal) ->
+                    this.coopBank = post(coop, this.coopBank, Event::CoopBank)
+                    this.personalBank = post(personal, this.personalBank, Event::Bank)
+                }
+                soulflowRegex.anyMatch(event.new, "soulflow") { (soulflow) ->
+                    this.soulflow = soulflow.parseFormattedLong()
+                }
+            }
+            TabWidget.AREA -> {
+                gemsRegex.anyMatch(event.new, "gems") { (gems) ->
+                    this.gems = post(gems, this.gems, Event::Gems)
+                }
+            }
+            else -> return
         }
     }
 
@@ -95,29 +115,35 @@ object CurrencyAPI {
         if (SkyBlockIsland.THE_RIFT.inIsland()) {
             motesRegex.anyFound(event.added, "motes") { (motes) ->
                 // Has a decimal place if obtained via mcgrubber burgers
-                this.motes = motes.parseFormattedLong()
+                this.motes = post(motes, this.motes, Event::Motes)
             }
         } else {
             if (SkyBlockIsland.JERRYS_WORKSHOP.inIsland()) {
                 northStarsRegex.anyFound(event.added, "northstars") { (northstars) ->
-                    this.northStars = northstars.parseFormattedLong()
+                    this.northStars = post(northstars, this.northStars, Event::NorthStars)
                 }
             } else if (SkyBlockIsland.GARDEN.inIsland()) {
                 copperRegex.anyFound(event.added, "copper") { (copper) ->
-                    this.copper = copper.parseFormattedLong()
+                    this.copper = post(copper, this.copper, Event::Copper)
                 }
             }
             purseRegex.anyFound(event.added, "type", "purse") { (type, purse) ->
-                this.purse = purse.parseFormattedDouble()
-                this.purseType = when (type.lowercase()) {
-                    "purse" -> PurseType.NORMAL
-                    "piggy" -> PurseType.PIGGY
-                    else -> PurseType.UNKNOWN
-                }
+                this.purseType = PurseType.fromName(type)
+                this.purse = post(purse.parseFormattedDouble(), this.purse, Event::Purse)
             }
             bitsRegex.anyFound(event.added, "bits") { (bits) ->
-                this.bits = bits.parseFormattedLong()
+                this.bits = post(bits, this.bits, Event::Bits)
             }
         }
+    }
+
+    // Helper method for specifically parsing longs from a string, since most of them are like this
+    private inline fun post(new: String, old: Long, event: (Long, Long) -> CurrencyUpdateEvent<Long>): Long {
+        return post(new.parseFormattedLong(), old, event)
+    }
+
+    private inline fun <N : Number> post(new: N, old: N, event: (N, N) -> CurrencyUpdateEvent<N>): N {
+        if (new != old) event(new, old).post()
+        return new
     }
 }
