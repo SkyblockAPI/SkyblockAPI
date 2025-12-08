3f3cf12907841008b764ad5a0612e550b3a202d8
Helium9 <80259183+sivthepolarfox@users.noreply.github.com>
2025-12-08 22:58:14 +0100

    mrrow

---
+++ src/common/main/kotlin/tech/thatgravyboat/skyblockapi/api/data/CrystalData.kt
@@ -0,0 +1,35 @@
+package tech.thatgravyboat.skyblockapi.api.data
+
+import tech.thatgravyboat.skyblockapi.api.remote.api.SkyBlockId
+import tech.thatgravyboat.skyblockapi.utils.extentions.toFormattedName
+import tech.thatgravyboat.skyblockapi.utils.text.Text
+import tech.thatgravyboat.skyblockapi.utils.text.TextColor
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
+
+enum class CrystalType(val textColor: Int, skyblockId: String? = null) {
+    JADE(TextColor.GREEN),
+    AMBER(TextColor.GOLD),
+    AMETHYST(TextColor.DARK_PURPLE),
+    SAPPHIRE(TextColor.AQUA),
+    TOPAZ(TextColor.YELLOW),
+    JASPER(TextColor.LIGHT_PURPLE),
+    RUBY(TextColor.RED),
+    OPAL(TextColor.WHITE),
+    AQUAMARINE(TextColor.DARK_BLUE),
+    PERIDOT(TextColor.DARK_GREEN),
+    ONYX(TextColor.DARK_GRAY),
+    CITRINE(TextColor.DARK_RED),
+    ;
+
+    val id = SkyBlockId.item(skyblockId ?: "${name.lowercase()}_crystal")
+
+    val displayName = Text.of("${toFormattedName()} Crystal") {
+        this.color = textColor
+    }
+}
+
+enum class CrystalStatus {
+    UNOBTAINED,
+    OBTAINED,
+    PLACED,
+}
