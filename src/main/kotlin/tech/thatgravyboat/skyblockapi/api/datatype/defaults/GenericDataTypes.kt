704d6914b281cb0564c4b24f4d119ebd21bfec38
heliguess <72938736+heliguess@users.noreply.github.com>
2026-01-24 19:07:47 +0100

    suggested change
    Co-authored-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockapi/api/datatype/defaults/GenericDataTypes.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockapi/api/datatype/defaults/GenericDataTypes.kt
@@ -55,7 +55,7 @@ object GenericDataTypes {
     val MIDAS_WEAPON_BID: DataType<Int> = DataType.simple("midas_weapon_bid", "winning_bid")
     val MIDAS_WEAPON_ADDED_COINS: DataType<Int> = DataType.simple("midas_weapon_added_coins", "additional_coins")
     val MIDAS_WEAPON_PAID: DataType<Long> = DataType.of("midas_weapon_paid") { stack ->
-        listOfNotNull(MIDAS_WEAPON_BID.factory(stack)?.toLong(), MIDAS_WEAPON_ADDED_COINS.factory(stack)?.toLong()).sum().takeUnless { it == 0L }
+        listOfNotNull(MIDAS_WEAPON_BID.factory(stack), MIDAS_WEAPON_ADDED_COINS.factory(stack)).sum().toLong().takeUnless { it == 0L }
     }
     val ENRICHMENT: DataType<SkyBlockId> = DataType.of("enrichment") {
         val id = it.tag?.getStringOrNull("talisman_enrichment") ?: return@of null
