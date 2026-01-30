8ee74e1131b66ed57c7d7bc5e9a120e53e904c0c
Gravy Boat <gravy@thatgravyboat.tech>
2026-01-30 07:32:27 -0330

    fix: bundle packets

--- src/main/java/tech/thatgravyboat/skyblockapi/mixins/events/ConnectionMixin.java
+++ src/main/java/tech/thatgravyboat/skyblockapi/mixins/events/ConnectionMixin.java
@@ -5,6 +5,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.network.Connection;
 import net.minecraft.network.PacketListener;
 import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundBundlePacket;
 import org.spongepowered.asm.mixin.Mixin;
 import tech.thatgravyboat.skyblockapi.api.SkyBlockAPI;
 import tech.thatgravyboat.skyblockapi.api.events.level.PacketReceivedEvent;
@@ -14,6 +15,8 @@ public class ConnectionMixin {
 
     @WrapMethod(method = "genericsFtw")
     private static void genericsFtw(Packet<?> packet, PacketListener listener, Operation<Void> original) {
+        // The bundle packets are special and are already handled in the BundleInfoMixin
+        if (packet instanceof ClientboundBundlePacket) return;
         if (!new PacketReceivedEvent(packet).post(SkyBlockAPI.getEventBus())) {
             original.call(packet, listener);
         }
