0940bc3bd380007da687c629e5ce809f4b35a322
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-02-22 11:13:56 +0100

    bump foojay
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
     id("dev.kikugie.stonecutter") version "0.8.3"
 }
 
