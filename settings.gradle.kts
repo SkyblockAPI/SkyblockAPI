4467d89300c3a3d0ba7685b3c1ae28e70c22e6aa
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-03-01 19:33:45 +0100

    bump sc
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("dev.kikugie.stonecutter") version "0.9-alpha.7"
+    id("dev.kikugie.stonecutter") version "0.9-beta.1"
 }
 
 val versions = listOf("26.1", "1.21.11", "1.21.10")
