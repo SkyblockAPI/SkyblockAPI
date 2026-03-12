73355e84b21d6d529b6a0e0085e62e4004377a7b
Mona <git@mona.cat>
2026-03-12 16:30:58 +0100

    chore: remove invariant values

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -19,10 +19,6 @@ stonecutter active "26.1"
 stonecutter parameters {
     swaps["mod_version"] = "\"" + property("version") + "\";"
     swaps["minecraft"] = "\"" + node.metadata.version + "\";"
-    replacements.string {
-        direction = eval(current.version, "> 1.21.5")
-        replace("// moj_import <", "//!moj_import <")
-    }
 
     filters.include("**/*.fsh", "**/*.vsh")
 
