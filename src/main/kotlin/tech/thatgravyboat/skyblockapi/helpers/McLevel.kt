c1aea7c41c325bba609b9ca03a5d42641a76a40f
Mona <git@mona.cat>
2026-03-18 16:08:02 +0100

    chore: kill McLevel#self and McLevel#level due to them returning unsafe values

--- src/main/kotlin/tech/thatgravyboat/skyblockapi/helpers/McLevel.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockapi/helpers/McLevel.kt
@@ -5,7 +5,7 @@ import net.minecraft.core.BlockPos
 import net.minecraft.core.RegistryAccess
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.player.Player
-import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.entity.EntityTypeTest
 import net.minecraft.world.phys.AABB
@@ -15,29 +15,23 @@ object McLevel {
     private val mutablePos = BlockPos.MutableBlockPos()
 
     val hasLevel: Boolean
-        get() = selfOrNull != null
+        get() = self != null
 
-    val self: Level
-        get() = level
-
-    val selfOrNull: ClientLevel?
+    val self: ClientLevel?
         get() = McClient.self.level
 
-    val level: ClientLevel
-        get() = McClient.self.level!!
-
     val registry: RegistryAccess
-        get() = self.registryAccess()
+        get() = self?.registryAccess() ?: RegistryAccess.EMPTY
 
-    operator fun get(pos: BlockPos): BlockState = self.getBlockState(pos)
-    operator fun get(x: Int, y: Int, z: Int): BlockState = self.getBlockState(mutablePos.set(x, y, z))
+    operator fun get(pos: BlockPos): BlockState = self?.getBlockState(pos) ?: Blocks.AIR.defaultBlockState()
+    operator fun get(x: Int, y: Int, z: Int): BlockState = self?.getBlockState(mutablePos.set(x, y, z)) ?: Blocks.AIR.defaultBlockState()
 
 
     val players: List<Player>
-        get() = selfOrNull?.players().orEmpty()
+        get() = self?.players().orEmpty()
 
     fun <E : Entity> getEntities(entityTypeTest: EntityTypeTest<Entity, E>, aabb: AABB, predicate: (E) -> Boolean = { true }): List<E> {
-        return selfOrNull?.getEntities(entityTypeTest, aabb, predicate).orEmpty()
+        return self?.getEntities(entityTypeTest, aabb, predicate).orEmpty()
     }
 
     inline fun <reified E : Entity> getEntities(aabb: AABB, noinline predicate: (E) -> Boolean = { true }): List<E> {
