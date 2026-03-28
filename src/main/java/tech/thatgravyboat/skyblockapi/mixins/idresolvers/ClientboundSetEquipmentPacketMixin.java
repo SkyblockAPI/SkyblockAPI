2779887811cfb0bc2164f0942bc95d7bdf8d8a2f
Mona <git@mona.cat>
2026-03-28 17:37:36 +0100

    feat: add sbid resolvers (#373)

---
+++ src/main/java/tech/thatgravyboat/skyblockapi/mixins/idresolvers/ClientboundSetEquipmentPacketMixin.java
@@ -0,0 +1,26 @@
+package tech.thatgravyboat.skyblockapi.mixins.idresolvers;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.game.ClientboundSetEquipmentPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import tech.thatgravyboat.skyblockapi.api.remote.api.SkyBlockId;
+import tech.thatgravyboat.skyblockapi.api.remote.api.resolvers.IdResolverKind;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+@Mixin(ClientboundSetEquipmentPacket.class)
+public class ClientboundSetEquipmentPacketMixin {
+
+    @WrapOperation(method = "<init>(Lnet/minecraft/network/RegistryFriendlyByteBuf;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/codec/StreamCodec;decode(Ljava/lang/Object;)Ljava/lang/Object;"))
+    private Object wrap(StreamCodec<?, ?> instance, Object o, Operation<Object> original) {
+        SkyBlockId.Companion.getIdResolverKind().set(IdResolverKind.Equipment);
+        var result = original.call(instance, o);
+        SkyBlockId.Companion.getIdResolverKind().set(IdResolverKind.Unknown);
+        return result;
+    }
+}
