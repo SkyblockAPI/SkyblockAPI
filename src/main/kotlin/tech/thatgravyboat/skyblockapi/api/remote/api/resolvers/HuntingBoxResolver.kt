374df8a7b67f97bd7240dd42a64b5afb8c86045f
Gravy Boat <gravy@thatgravyboat.tech>
2026-04-15 23:18:00 -0230

    chore: remove generic name resolver

---
+++ src/main/kotlin/tech/thatgravyboat/skyblockapi/api/remote/api/resolvers/HuntingBoxResolver.kt
@@ -0,0 +1,29 @@
+package tech.thatgravyboat.skyblockapi.api.remote.api.resolvers
+
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
+import net.minecraft.world.inventory.AbstractContainerMenu
+import net.minecraft.world.item.ItemStack
+import tech.thatgravyboat.skyblockapi.api.remote.api.SimpleItemAPI
+import tech.thatgravyboat.skyblockapi.api.remote.api.SkyBlockId
+import tech.thatgravyboat.skyblockapi.utils.extentions.cleanName
+import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
+
+@IdResolvers
+object HuntingBoxResolver : InventoryIdResolver {
+
+    override fun <T : AbstractContainerMenu> ItemStack.isApplicable(
+        menu: AbstractContainerScreen<T>,
+        resolverKind: IdResolverKind,
+    ): Boolean {
+        return menu.title.stripped == "Hunting Box"
+    }
+
+    override fun <T : AbstractContainerMenu> ItemStack.resolveId(
+        menu: AbstractContainerScreen<T>,
+        resolverKind: IdResolverKind,
+    ): SkyBlockId? {
+        return SimpleItemAPI.findIdByName(this.cleanName)?.takeIf(SkyBlockId::isAttribute)
+    }
+
+    override val priority: Int = 10
+}
