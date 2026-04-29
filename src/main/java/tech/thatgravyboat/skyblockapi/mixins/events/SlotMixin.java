86ea016825e173fd38f9382bedee089fe61ff8bc
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-04-29 19:08:11 +0200

    feat: add inventory slot to playerinventorychange/playerhotbarchange events
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/java/tech/thatgravyboat/skyblockapi/mixins/events/SlotMixin.java
+++ src/main/java/tech/thatgravyboat/skyblockapi/mixins/events/SlotMixin.java
@@ -4,7 +4,6 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,11 +13,10 @@ import tech.thatgravyboat.skyblockapi.api.events.screen.PlayerHotbarChangeEvent;
 import tech.thatgravyboat.skyblockapi.api.events.screen.PlayerInventoryChangeEvent;
 import tech.thatgravyboat.skyblockapi.helpers.McScreen;
 
+import static tech.thatgravyboat.skyblockapi.api.events.screen.PlayerHotbarChangeEvent.FIRST_HOTBAR_SLOT;
+
 @Mixin(Slot.class)
 public class SlotMixin {
-
-    @Unique
-    private static final int FIRST_HOTBAR_SLOT = 36;
 
     @Inject(method = "set", at = @At("HEAD"))
     public void set(ItemStack itemStack, CallbackInfo ci) {
@@ -27,9 +25,9 @@ public class SlotMixin {
         var isInventory = slot.container instanceof Inventory;
         var slotIndex = slot.index;
         if (isInventory) {
-            new PlayerInventoryChangeEvent(slotIndex, itemStack).post(SkyBlockAPI.getEventBus());
+            new PlayerInventoryChangeEvent(slot, itemStack).post(SkyBlockAPI.getEventBus());
             if (slotIndex >= FIRST_HOTBAR_SLOT && slotIndex < FIRST_HOTBAR_SLOT + 9) {
-                new PlayerHotbarChangeEvent(slotIndex - FIRST_HOTBAR_SLOT, itemStack).post(SkyBlockAPI.getEventBus());
+                new PlayerHotbarChangeEvent(slot, itemStack).post(SkyBlockAPI.getEventBus());
             }
         }
         if (menuScreen != null && (isInventory || menuScreen.getMenu().isValidSlotIndex(slotIndex))) {
