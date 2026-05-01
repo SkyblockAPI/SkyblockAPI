bb6d2a828e346dc81bc5649f9868276e6847b49b
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-05-01 13:56:07 +0200

    feat: add inventory slot to playerinventorychange/playerhotbarchange events (#394)

--- src/main/kotlin/tech/thatgravyboat/skyblockapi/api/events/screen/PlayerInventoryChangeEvent.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockapi/api/events/screen/PlayerInventoryChangeEvent.kt
@@ -2,13 +2,32 @@ package tech.thatgravyboat.skyblockapi.api.events.screen
 
 import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.player.Player
+import net.minecraft.world.inventory.Slot
 import net.minecraft.world.item.ItemStack
 import tech.thatgravyboat.skyblockapi.api.events.base.SkyBlockEvent
 
-class PlayerInventoryChangeEvent(val slot: Int, val item: ItemStack) : SkyBlockEvent()
+data class PlayerInventoryChangeEvent(val inventorySlot: Slot, val item: ItemStack) : SkyBlockEvent() {
+    //? < 26.1 {
+    @Deprecated("Use slotIndex instead", ReplaceWith("slotIndex"))
+    val slot get() = slotIndex
 
-class PlayerHotbarChangeEvent(val slot: Int, val item: ItemStack) : SkyBlockEvent()
+    //? }
+    val slotIndex get() = inventorySlot.index
+}
 
-class PlayerEquipmentChangeEvent(val entity: Player, val slot: EquipmentSlot, val previous: ItemStack, val current: ItemStack) : SkyBlockEvent() {
+data class PlayerHotbarChangeEvent(val inventorySlot: Slot, val item: ItemStack) : SkyBlockEvent() {
+    //? < 26.1 {
+    @Deprecated("Use slotIndex instead", ReplaceWith("slotIndex"))
+    val slot get() = slotIndex
+
+    //? }
+    val slotIndex get() = inventorySlot.index - FIRST_HOTBAR_SLOT
+
+    companion object {
+        internal const val FIRST_HOTBAR_SLOT = 36
+    }
+}
+
+data class PlayerEquipmentChangeEvent(val entity: Player, val slot: EquipmentSlot, val previous: ItemStack, val current: ItemStack) : SkyBlockEvent() {
     val item get() = current
 }
