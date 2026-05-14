f62fa92b7bc152b4826c4c63172562c7b91b69f8
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-05-14 20:22:37 +0200

    fix: new quiver api (#399)
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockapi/api/profile/quiver/QuiverAPI.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockapi/api/profile/quiver/QuiverAPI.kt
@@ -7,11 +7,27 @@ import tech.thatgravyboat.skyblockapi.api.data.stored.QuiverStorage
 import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
 import tech.thatgravyboat.skyblockapi.api.datatype.getData
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.misc.DebugBuilder
 import tech.thatgravyboat.skyblockapi.api.events.screen.ContainerInitializedEvent
 import tech.thatgravyboat.skyblockapi.api.events.screen.InventoryChangeEvent
 import tech.thatgravyboat.skyblockapi.api.events.screen.PlayerHotbarChangeEvent
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.bank
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.bits
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.coopBank
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.copper
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.gems
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.kernels
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.motes
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.northStars
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.personalBank
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.purse
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.purseType
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.soulflow
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI.sowdust
 import tech.thatgravyboat.skyblockapi.api.repo.apis.SkyBlockItemsRepo
+import tech.thatgravyboat.skyblockapi.utils.ApiDebug
 import tech.thatgravyboat.skyblockapi.utils.extentions.addOrPut
+import tech.thatgravyboat.skyblockapi.utils.extentions.cleanName
 import tech.thatgravyboat.skyblockapi.utils.extentions.getRawLore
 import tech.thatgravyboat.skyblockapi.utils.extentions.toIntValue
 import tech.thatgravyboat.skyblockapi.utils.regex.RegexGroup
@@ -23,9 +39,9 @@ object QuiverAPI {
 
     private val inventoryGroup = RegexGroup.INVENTORY.group("quiver")
 
-    private val activeArrowRegex = inventoryGroup.create(
+    private val arrowCountRegex = inventoryGroup.create(
         "active",
-        "^Active Arrow: (?<type>.+) \\((?i)(?<amount>[\\d,.kmb]+)\\)$"
+        "Arrows Remaining: (?<amount>[\\d,.kmb]+)",
     )
     private val quiverInventoryRegex = inventoryGroup.create(
         "inventory",
@@ -52,11 +68,11 @@ object QuiverAPI {
 
     @Subscription
     fun onHotbarChange(event: PlayerHotbarChangeEvent) {
-        if (event.slot != 8) return
+        if (event.slotIndex != 8) return
         val item = event.item
         if (item.getData(DataTypes.QUIVER_ARROW) != true) return
-        activeArrowRegex.anyFound(item.getRawLore(), "type", "amount") { (type, amount) ->
-            currentArrow = SkyBlockItemsRepo.getIdByName(type)
+        arrowCountRegex.anyFound(item.getRawLore(), "amount") { (amount) ->
+            currentArrow = SkyBlockItemsRepo.getIdByName(item.cleanName)
             if (currentArrow == null) return@anyFound
             currentAmount = amount.toIntValue()
         }
@@ -84,6 +100,15 @@ object QuiverAPI {
             }
         }
         QuiverStorage.setArrows(newArrows)
+    }
+
+    @ApiDebug("Quiver")
+    internal fun debug(builder: DebugBuilder) = with(builder) {
+        fields(
+            ::currentArrow,
+            ::currentAmount,
+            ::arrows,
+        )
     }
 
 }
