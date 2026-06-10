e83b58f8504ab1426f1aec25bc1a3ad6f72cd124
Mona <git@mona.cat>
2026-06-10 18:55:53 +0200

    fix: missing import :3

--- src/main/java/tech/thatgravyboat/skyblockapi/mixins/debug/DebugItemsMixin.java
+++ src/main/java/tech/thatgravyboat/skyblockapi/mixins/debug/DebugItemsMixin.java
@@ -9,6 +9,8 @@ import net.minecraft.core.component.PatchedDataComponentMap;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+//? < 26.1
+//import net.minecraft.world.level.ItemLike;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
