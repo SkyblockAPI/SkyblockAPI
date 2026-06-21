7ca9f40665d3427365238d072885099fdca0e421
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-06-21 18:13:16 +0200

    fix: binary incompat in player(hotbar/inventory)changeevent
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockapi/api/events/screen/PlayerInventoryChangeEvent.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockapi/api/events/screen/PlayerInventoryChangeEvent.kt
@@ -7,10 +7,14 @@ import net.minecraft.world.item.ItemStack
 import tech.thatgravyboat.skyblockapi.api.events.base.SkyBlockEvent
 
 data class PlayerInventoryChangeEvent(val inventorySlot: Slot, val item: ItemStack) : SkyBlockEvent() {
+    //? < 26.2
+    //val slot get() = slotIndex
     val slotIndex get() = inventorySlot.index
 }
 
 data class PlayerHotbarChangeEvent(val inventorySlot: Slot, val item: ItemStack) : SkyBlockEvent() {
+    //? < 26.2
+    //val slot get() = slotIndex
     val slotIndex get() = inventorySlot.index - FIRST_HOTBAR_SLOT
 
     companion object {
