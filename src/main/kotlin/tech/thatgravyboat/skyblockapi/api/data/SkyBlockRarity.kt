3e569154a0ef4496534fb6ace3268fc31140a8c9
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-07-04 13:39:10 +0200

    fix: DIVINE/SUPREME rarity detection
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockapi/api/data/SkyBlockRarity.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockapi/api/data/SkyBlockRarity.kt
@@ -6,14 +6,14 @@ import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
 
-enum class SkyBlockRarity(val color: Int) {
+enum class SkyBlockRarity(val color: Int, vararg val alternativeNames: String) {
     COMMON(TextColor.WHITE),
     UNCOMMON(TextColor.GREEN),
     RARE(TextColor.BLUE),
     EPIC(TextColor.DARK_PURPLE),
     LEGENDARY(TextColor.GOLD),
     MYTHIC(TextColor.LIGHT_PURPLE),
-    DIVINE(TextColor.AQUA),
+    DIVINE(TextColor.AQUA, "SUPREME"),
     ULTIMATE(TextColor.DARK_RED),
     SPECIAL(TextColor.RED),
     VERY_SPECIAL(TextColor.RED),
@@ -26,7 +26,7 @@ enum class SkyBlockRarity(val color: Int) {
     override fun toString(): String = displayName
 
     companion object {
-        fun fromNameOrNull(name: String) = entries.find { it.name.equals(name, true) }
+        fun fromNameOrNull(name: String) = entries.find { r -> r.name.equals(name, true) || r.alternativeNames.any { name.equals(it, true) } }
         fun fromName(name: String) = fromNameOrNull(name) ?: COMMON
 
         internal fun fromColorOrNull(colorCode: Int): SkyBlockRarity? {
