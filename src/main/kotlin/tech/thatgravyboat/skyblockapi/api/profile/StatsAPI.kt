ddd3ed59d5708688a89ebf90538bb8c6f77a501c
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-08-13 19:33:32 +0200

    refactor: add derprecation
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockapi/api/profile/StatsAPI.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockapi/api/profile/StatsAPI.kt
@@ -2,13 +2,7 @@ package tech.thatgravyboat.skyblockapi.api.profile
 
 import me.owdding.ktmodules.Module
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
-import tech.thatgravyboat.skyblockapi.api.events.info.ActionBarWidgetChangeEvent
-import tech.thatgravyboat.skyblockapi.api.events.info.ArmadilloActionBarWidgetChangeEvent
-import tech.thatgravyboat.skyblockapi.api.events.info.DefenseActionBarWidgetChangeEvent
-import tech.thatgravyboat.skyblockapi.api.events.info.HealthActionBarWidgetChangeEvent
-import tech.thatgravyboat.skyblockapi.api.events.info.ManaActionBarWidgetChangeEvent
-import tech.thatgravyboat.skyblockapi.api.events.info.OverflowManaActionBarWidgetChangeEvent
-import tech.thatgravyboat.skyblockapi.api.events.info.VitalityActionBarWidgetChangeEvent
+import tech.thatgravyboat.skyblockapi.api.events.info.*
 import tech.thatgravyboat.skyblockapi.api.location.LocationAPI
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
 import tech.thatgravyboat.skyblockapi.helpers.McPlayer
@@ -40,7 +34,9 @@ object StatsAPI {
     var maxVitality: Int = 100
         private set
 
+    @Deprecated("Use vitality instead", ReplaceWith("vitality"))
     val vitaliy: Int get() = vitality
+    @Deprecated("Use maxVitality instead", ReplaceWith("maxVitality"))
     val maxVitaliy: Int get() = maxVitality
 
     @Subscription
