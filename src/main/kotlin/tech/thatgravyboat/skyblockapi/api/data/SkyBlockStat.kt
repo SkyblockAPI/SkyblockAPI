89186b931ef6a6d49046eb3585ecf8f6912d1658
JustAlex <JustAlex7473@gmail.com>
2026-08-14 05:54:49 -0400

    feat(SkyblockStat): stats from 0.27 (#449)

--- src/main/kotlin/tech/thatgravyboat/skyblockapi/api/data/SkyBlockStat.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockapi/api/data/SkyBlockStat.kt
@@ -66,6 +66,7 @@ enum class SkyBlockStat(
     FORAGING_FORTUNE(GENERIC_FORAGING_FORTUNE),
     FIG_FORTUNE(GENERIC_FORAGING_FORTUNE),
     MANGROVE_FORTUNE(GENERIC_FORAGING_FORTUNE),
+    HELIX_FORTUNE(GENERIC_FORAGING_FORTUNE),
 
     // Fishing Stats
     FISHING_SPEED('', TextColor.AQUA),
@@ -87,7 +88,8 @@ enum class SkyBlockStat(
 
     // Hunting Stats
     PULL('', TextColor.AQUA),
-    HUNTER_FORTUNE('', TextColor.LIGHT_PURPLE),
+    HUNTING_FORTUNE('', TextColor.LIGHT_PURPLE),
+    CHARM_CHANCE('❣', TextColor.AQUA),
 
     // Wisdom Stats
     GENERIC_WISDOM('☯', TextColor.DARK_AQUA), // helper
@@ -107,8 +109,12 @@ enum class SkyBlockStat(
     // Misc Stats
     @Deprecated("Unused in game, use TROPHY_CHANCE instead.")
     TROPHY_FISH_CHANCE('♔', TextColor.GOLD),
-    @Deprecated("Unused in game, use HUNTER_FORTUNE instead.")
+
+    @Deprecated("Unused in game, use HUNTING_FORTUNE instead.")
     SYPHON_LUCK('♣', TextColor.LIGHT_PURPLE),
+
+    @Deprecated("Unused in game, use HUNTING_FORTUNE instead.")
+    HUNTER_FORTUNE('', TextColor.LIGHT_PURPLE),
     ;
 
     constructor(stat: SkyBlockStat) : this(stat.icon, stat.color)
