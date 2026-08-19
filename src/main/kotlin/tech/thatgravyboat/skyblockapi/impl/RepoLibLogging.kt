529da28cb637dfbba529cf6de48287bea9cf4d94
Mona <git@mona.cat>
2026-08-19 21:05:00 +0200

    chore: add overwrite log level

--- src/main/kotlin/tech/thatgravyboat/skyblockapi/impl/RepoLibLogging.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockapi/impl/RepoLibLogging.kt
@@ -1,7 +1,5 @@
 package tech.thatgravyboat.skyblockapi.impl
 
-import org.apache.logging.log4j.Level
-import org.apache.logging.log4j.core.config.Configurator
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import tech.thatgravyboat.repolib.api.RepoLibLogger
@@ -10,20 +8,18 @@ import tech.thatgravyboat.skyblockapi.utils.debugToggle
 object RepoLibLogging : RepoLibLogger, Logger by LoggerFactory.getLogger("Repo-Lib") {
     val enableDebugLogging by debugToggle("repo_lib_debug_logging")
 
-    private inline fun updateLogLevel(runnable: () -> Unit) {
+    private inline fun updateLogLevel(value: String, runnable: (String) -> Unit, debugLogger: (String) -> Unit = ::info) {
         if (enableDebugLogging) {
-            Configurator.setLevel(name, Level.TRACE)
+            debugLogger(value)
         } else {
-            Configurator.setLevel(name, Level.INFO)
+            runnable(value)
         }
-
-        runnable()
     }
 
-    override fun info0(p0: String) = updateLogLevel { info(p0) }
-    override fun debug0(p0: String) = updateLogLevel { debug(p0) }
-    override fun trace0(p0: String) = updateLogLevel { trace(p0) }
-    override fun error0(p0: String) = updateLogLevel { error(p0) }
-    override fun warn0(p0: String) = updateLogLevel { warn(p0) }
-    override fun error0(p0: String, p1: Throwable) = updateLogLevel { error(p0, p1) }
+    override fun info0(p0: String) = updateLogLevel(p0, this::info)
+    override fun debug0(p0: String) = updateLogLevel(p0, this::debug)
+    override fun trace0(p0: String) = updateLogLevel(p0, this::trace)
+    override fun error0(p0: String) = updateLogLevel(p0, this::error, this::error)
+    override fun warn0(p0: String) = updateLogLevel(p0, this::warn, this::warn)
+    override fun error0(p0: String, p1: Throwable) = error(p0, p1)
 }
