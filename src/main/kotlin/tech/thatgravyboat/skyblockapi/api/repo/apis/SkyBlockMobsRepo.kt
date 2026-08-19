d1303e4706511cb1125cd86afdf454abfa9a9159
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-08-19 18:57:26 +0200

    feat: add fake ids to SkyBlockMobsRepo
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockapi/api/repo/apis/SkyBlockMobsRepo.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockapi/api/repo/apis/SkyBlockMobsRepo.kt
@@ -1,16 +1,34 @@
 package tech.thatgravyboat.skyblockapi.api.repo.apis
 
+import net.minecraft.core.component.DataComponents
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.world.item.ItemStack
 import tech.thatgravyboat.repolib.api.RepoAPI
 import tech.thatgravyboat.repolib.api.mobs.LootTable
 import tech.thatgravyboat.repolib.api.mobs.Mob
 import tech.thatgravyboat.skyblockapi.api.repo.LazyItemStack
+import tech.thatgravyboat.skyblockapi.utils.extentions.compoundTag
+import tech.thatgravyboat.skyblockapi.utils.extentions.toData
+import kotlin.jvm.optionals.getOrNull
 
 object SkyBlockMobsRepo : RepoItemCache<String>("Mobs") {
 
+    private const val ID_KEY = "skyblock-api:id"
     private val repo get() = RepoAPI.mobs()
 
-    override fun create(key: String): LazyItemStack? = get(key)?.item?.let(::LazyItemStack)
+    override fun create(key: String): LazyItemStack? {
+        val baseItem = get(key)?.item?.let(::LazyItemStack) ?: return null
+
+        return baseItem.withComponents {
+            val existingTag = baseItem[DataComponents.CUSTOM_DATA]?.copyTag() ?: CompoundTag()
+            existingTag.putString(ID_KEY, key)
+            this[DataComponents.CUSTOM_DATA] = existingTag.toData()
+        }
+    }
 
     fun get(key: String): Mob? = this.repo.getMob(key)
     fun getLootTables(key: String): List<LootTable> = get(key)?.lootTables ?: emptyList()
+
+    fun ItemStack.getMobId(): String? = this.get(DataComponents.CUSTOM_DATA)?.copyTag()?.getString(ID_KEY)?.getOrNull()
+    fun LazyItemStack.getMobId(): String? = this[DataComponents.CUSTOM_DATA]?.copyTag()?.getString(ID_KEY)?.getOrNull()
 }
