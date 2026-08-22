bcb561255b51961f38d972bea69bd444342b0edd
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-08-22 13:30:26 +0200

    feat: dowsing mode datatype
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skyblockapi/api/datatype/DataTypes.kt
+++ src/main/kotlin/tech/thatgravyboat/skyblockapi/api/datatype/DataTypes.kt
@@ -57,6 +57,7 @@ object DataTypes {
     val POTION_LEVEL: DataType<Int> = GenericDataTypes.POTION_LEVEL
     val BOOK_OF_STATS: DataType<Int> = GenericDataTypes.BOOK_OF_STATS
     val RUNEBOOK: DataType<Int> = GenericDataTypes.RUNEBOOK
+    val DOWSING_MODE: DataType<String> = GenericDataTypes.DOWSING_MODE
 
     val USED_RUNE: DataType<SkyBlockId> = GenericDataTypes.USED_RUNE
     val HELMET_SKIN: DataType<String> = GenericDataTypes.HELMET_SKIN
